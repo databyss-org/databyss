0e17af104e12755e7a65daf71c0ba6364c713708
Michael <michaelsenejoa@gmail.com>
2019-08-27 18:37:15 -0400

    run serially for api-tests

--- packages/databyss-api/routes/api/helpers/pagesHelper.js
+++ packages/databyss-api/routes/api/helpers/pagesHelper.js
@@ -4,22 +4,25 @@ const Entry = require('../../../models/Entry')
 
 const getBlockItemsFromId = blocks => {
   const promises = blocks.map(async b => {
-    const _id = b._id
+    const _id = b._id.toString()
     const block = await Block.findOne({
       _id,
     }).catch(err => console.log(err))
-    const { type, entryId, sourceId, authorId } = block
-    const response = { type, _id }
-    if (type === 'ENTRY') {
-      response.refId = entryId
+    if (block) {
+      const { type, entryId, sourceId, authorId } = block
+      const response = { type, _id }
+      if (type === 'ENTRY') {
+        response.refId = entryId
+      }
+      if (type === 'SOURCE') {
+        response.refId = sourceId
+      }
+      if (type === 'AUTHOR') {
+        response.refId = authorId
+      }
+      return response
     }
-    if (type === 'SOURCE') {
-      response.refId = sourceId
-    }
-    if (type === 'AUTHOR') {
-      response.refId = authorId
-    }
-    return response
+    return {}
   })
   return Promise.all(promises)
 }
@@ -53,7 +56,9 @@ const getEntriesFromId = list => {
 const dictionaryFromList = list => {
   const result = {}
   list.forEach(b => {
-    result[b._id] = b
+    if (b) {
+      result[b._id] = b
+    }
   })
   return result
 }
