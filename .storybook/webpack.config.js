f97e2d5537a81b52ce9839dbf9c16520e29089cc
pzhine <paul@hine.works>
2019-09-11 11:59:50 -0400

    fix weird storybook:build bug (see PR notes)

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -8,11 +8,12 @@
 
 const webpack = require('../config/webpack.config.js')('development')
 
-module.exports = {
-  plugins: [
-    // your custom plugins
-  ],
-  module: {
-    rules: webpack.module.rules,
-  },
+module.exports = async ({ config, mode }) => {
+  // `mode` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+  // You can change the configuration based on that.
+  // 'PRODUCTION' is used when building the static version of storybook.
+
+  config.module.rules = webpack.module.rules
+
+  return config
 }
