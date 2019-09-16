bd54d04b59cac44537cbfba917c934d7e7f64b61
pzhine <paul@hine.works>
2019-09-16 14:12:28 -0400

    configure cypress headless run for CI

--- packages/databyss-ui/components/Editor/slate/__tests__/editable.cypress.js
+++ packages/databyss-ui/components/Editor/slate/__tests__/editable.cypress.js
@@ -1,13 +1,75 @@
+/** @jsx h */
+
+/* eslint-disable func-names */
+
+import h from 'slate-hyperscript'
+import { toSlateJson, matchExpectedJson } from './_helpers'
+
 context('Editor', () => {
   beforeEach(() => {
     cy.visit('http://0.0.0.0:6006/iframe.html?id=editor-tests--slate')
+    cy.get('[contenteditable="true"]')
+      .as('editor')
+      .focus()
+    cy.get('#slateDocument').as('slateDocument')
   })
 
-  // https://on.cypress.io/interacting-with-elements
   it('renders the contenteditable container', () => {
-    // https://on.cypress.io/focus
-    cy.get('[contenteditable="true"]')
-      .focus()
-      .should('have.attr', 'role')
+    cy.get('@editor').should('have.attr', 'role')
+  })
+
+  it('should set initial blocks and inlines', () => {
+    const expected = toSlateJson(
+      <value>
+        <document>
+          <block type="SOURCE">
+            <text />
+            <inline type="SOURCE">
+              Stamenov, Language Structure, Discourse and the Access to
+              Consciousness
+            </inline>
+            <text />
+          </block>
+          <block type="ENTRY">
+            On the limitation of third-order thought to assertion
+          </block>
+        </document>
+      </value>
+    )
+    cy.get('@slateDocument').then(matchExpectedJson(expected.document))
+  })
+
+  it('should set @ block to SOURCE on blur', () => {
+    cy.get('@editor')
+      .type('{downarrow}')
+      .type('{downarrow}')
+      .type('{downarrow}')
+      .type('{downarrow}')
+      .type('{home}')
+      .type('@')
+      .type('{uparrow}')
+
+    const expected = toSlateJson(
+      <value>
+        <document>
+          <block type="SOURCE">
+            <text />
+            <inline type="SOURCE">
+              Stamenov, Language Structure, Discourse and the Access to
+              Consciousness
+            </inline>
+            <text />
+          </block>
+          <block type="SOURCE">
+            <text />
+            <inline type="SOURCE">
+              On the limitation of third-order thought to assertion
+            </inline>
+            <text />
+          </block>
+        </document>
+      </value>
+    )
+    cy.get('@slateDocument').then(matchExpectedJson(expected.document))
   })
 })
