94a39f2222acfc030c6043be82d117a44ec3e159
Michael <michaelsenejoa@gmail.com>
2019-10-03 10:57:59 -0400

    locations

--- packages/databyss-ui/components/Editor/slate/ContentEditable.js
+++ packages/databyss-ui/components/Editor/slate/ContentEditable.js
@@ -174,33 +174,6 @@ const SlateContentEditable = ({
       let rawHtml = ''
       if (_nextEditableState.value.document.getNode(activeBlockId)) {
         rawHtml = _nextEditableState.value.document.getNode(activeBlockId).text
-        // if not atomic get range and check for location
-        if (
-          !isAtomicInlineType(
-            _nextEditableState.value.document.getNode(activeBlockId).type
-          )
-        ) {
-          const ranges = getBlockRanges(
-            _nextEditableState.value.document.getNode(activeBlockId)
-          )
-
-          const locationLength = ranges.reduce((acc, range) => {
-            if (range.marks.findIndex(m => m === 'location') > -1) {
-              return range.length + acc
-            }
-            return acc
-          }, 0)
-
-          // if whole entry has a location range set block as LOCATION
-          if (
-            rawHtml.length !== 0 &&
-            locationLength === rawHtml.length &&
-            _nextEditableState.value.document.getNode(activeBlockId).type !==
-              'LOCATION'
-          ) {
-            onSetBlockType('LOCATION', activeBlockId, _nextEditableState)
-          }
-        }
       }
 
       onBlockBlur(activeBlockId, rawHtml, _nextEditableState)
@@ -236,9 +209,40 @@ const SlateContentEditable = ({
     if (_nextText !== _prevText) {
       const block = _nextEditableState.value.anchorBlock
       const ranges = getBlockRanges(block)
-      // const block = _nextEditableState.value.anchorBlock
-      // const jsonBlockValue = { ...block.toJSON(), key: block.key }
-      // const ranges = getRangesFromBlock(jsonBlockValue).ranges
+
+      // if not atomic get range and check for location
+      if (
+        !isAtomicInlineType(
+          _nextEditableState.value.document.getNode(activeBlockId).type
+        )
+      ) {
+        const locationLength = ranges.reduce((acc, range) => {
+          if (range.marks.findIndex(m => m === 'location') > -1) {
+            return range.length + acc
+          }
+          return acc
+        }, 0)
+
+        // if type LOCATION is set check for non LOCATION type
+        if (
+          _nextEditableState.value.document.getNode(activeBlockId).type ===
+            'LOCATION' &&
+          locationLength !== _nextText.length
+        ) {
+          onSetBlockType('ENTRY', activeBlockId, _nextEditableState)
+        }
+
+        // if whole entry has a location range set block as LOCATION
+        if (
+          _nextText.length !== 0 &&
+          locationLength === _nextText.length &&
+          _nextEditableState.value.document.getNode(activeBlockId).type !==
+            'LOCATION'
+        ) {
+          onSetBlockType('LOCATION', activeBlockId, _nextEditableState)
+        }
+      }
+
       onActiveBlockContentChange(_nextText, _nextEditableState, ranges)
       return true
     }
