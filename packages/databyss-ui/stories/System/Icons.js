b61d878e63412b3ed3e56faad28bfead51b90501
pzhine <paul@hine.works>
2019-10-04 18:12:50 +0200

    icon assets for sidebar menu

--- packages/databyss-ui/stories/System/Icons.js
+++ packages/databyss-ui/stories/System/Icons.js
@@ -1,6 +1,13 @@
 import React from 'react'
 import { View, Icon, Text } from '@databyss-org/ui/primitives'
 import SourceSvg from '@databyss-org/ui/assets/source.svg'
+import AuthorSvg from '@databyss-org/ui/assets/author.svg'
+import ChevronSvg from '@databyss-org/ui/assets/chevron.svg'
+import EditSvg from '@databyss-org/ui/assets/edit.svg'
+import FilterSvg from '@databyss-org/ui/assets/filter.svg'
+import PageSvg from '@databyss-org/ui/assets/page.svg'
+import SearchSvg from '@databyss-org/ui/assets/search.svg'
+import TopicSvg from '@databyss-org/ui/assets/topic.svg'
 import { Grid } from '@databyss-org/ui'
 import { Section } from './'
 
@@ -29,7 +36,7 @@ export default () => (
         <SourceSvg />
       </LabeledIcon>
     </Grid>
-    <Grid mb="small" rowGap="medium" columnGap="medium">
+    <Grid mb="medium" rowGap="medium" columnGap="medium">
       <LabeledIcon label="Default" sizeVariant="small">
         <SourceSvg />
       </LabeledIcon>
@@ -41,6 +48,32 @@ export default () => (
       </LabeledIcon>
       <LabeledIcon label="Primary" color="primary.0" sizeVariant="small">
         <SourceSvg />
+      </LabeledIcon>
+    </Grid>
+    <Grid mb="medium" rowGap="medium" columnGap="medium">
+      <LabeledIcon label="Author" sizeVariant="small">
+        <AuthorSvg />
+      </LabeledIcon>
+      <LabeledIcon label="Chevron" sizeVariant="small">
+        <ChevronSvg />
+      </LabeledIcon>
+      <LabeledIcon label="Edit" sizeVariant="small">
+        <EditSvg />
+      </LabeledIcon>
+      <LabeledIcon label="Filter" sizeVariant="small">
+        <FilterSvg />
+      </LabeledIcon>
+      <LabeledIcon label="Page" sizeVariant="small">
+        <PageSvg />
+      </LabeledIcon>
+      <LabeledIcon label="Search" sizeVariant="small">
+        <SearchSvg />
+      </LabeledIcon>
+      <LabeledIcon label="Source" sizeVariant="small">
+        <SourceSvg />
+      </LabeledIcon>
+      <LabeledIcon label="Topic" sizeVariant="small">
+        <TopicSvg />
       </LabeledIcon>
     </Grid>
   </Section>
