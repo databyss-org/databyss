66c64cb6a1de2006b20e3fb9a7e7e54529488c02
Michael <michaelsenejoa@gmail.com>
2019-11-21 13:51:48 -0500

    style

--- packages/databyss-ui/stories/Forms/forms.stories.js
+++ packages/databyss-ui/stories/Forms/forms.stories.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react'
 import { storiesOf } from '@storybook/react'
 import ObjectId from 'bson-objectid'
-
 import ValueListProvider, {
   ValueListItem,
 } from '@databyss-org/ui/components/ValueList/ValueListProvider'
 import { View, TextControl, List, Grid } from '@databyss-org/ui/primitives'
+import Section from './../Section'
+
 import { ViewportDecorator } from '../decorators'
 
 const _id = ObjectId().toHexString()
@@ -40,105 +41,76 @@ const SourceForm = () => {
 
   return (
     <ValueListProvider onChange={setValues} values={values}>
-      <Grid>
-        <View
-          id="formDocuments"
-          borderVariant="thinDark"
-          paddingVariant="none"
-          overflow="scroll"
-          maxWidth="500px"
-          flexShrink={1}
-        >
-          <pre>{JSON.stringify(values, null, 2)}</pre>
-        </View>
-        <View
-          borderVariant="thinLight"
-          widthVariant="content"
-          backgroundColor="background.0"
-        >
-          <ControlList verticalItemPadding="tiny">
-            <ValueListItem textPath="name" rangesPath="ranges">
-              <TextControl
-                labelProps={{
-                  width: '25%',
-                }}
-                label="Citation"
-                gridFlexWrap="nowrap"
-                multiline
-                rich
-              />
-            </ValueListItem>
-            <ValueListItem textPath="authors[0].firstName">
-              <TextControl
-                labelProps={{
-                  width: '25%',
-                }}
-                label="Author (First Name)"
-                gridFlexWrap="nowrap"
-              />
-            </ValueListItem>
-            <ValueListItem textPath="authors[0].lastName">
-              <TextControl
-                labelProps={{
-                  width: '25%',
-                }}
-                label="Author (Last Name)"
-                gridFlexWrap="nowrap"
-              />
-            </ValueListItem>
-            <ValueListItem
-              textPath="citations[0].textValue"
-              rangesPath="citations[0].ranges"
-            >
-              <TextControl
-                labelProps={{
-                  width: '25%',
-                }}
-                label="Citation"
-                rich
-                gridFlexWrap="nowrap"
-                multiline
-              />
-            </ValueListItem>
-          </ControlList>
-        </View>
-      </Grid>
+      <Section title="Value List">
+        <Grid>
+          <View
+            id="formDocuments"
+            borderVariant="thinDark"
+            paddingVariant="none"
+            overflow="scroll"
+            maxWidth="500px"
+            flexShrink={1}
+          >
+            <pre>{JSON.stringify(values, null, 2)}</pre>
+          </View>
+          <View
+            borderVariant="thinLight"
+            paddingVariant="small"
+            widthVariant="content"
+            backgroundColor="background.0"
+          >
+            <ControlList verticalItemPadding="tiny">
+              <ValueListItem textPath="name" rangesPath="ranges">
+                <TextControl
+                  labelProps={{
+                    width: '25%',
+                  }}
+                  label="Citation"
+                  multiline
+                  paddingVariant="tiny"
+                  rich
+                />
+              </ValueListItem>
+              <ValueListItem textPath="authors[0].firstName">
+                <TextControl
+                  labelProps={{
+                    width: '25%',
+                  }}
+                  label="Author (First Name)"
+                  gridFlexWrap="nowrap"
+                  paddingVariant="tiny"
+                />
+              </ValueListItem>
+              <ValueListItem textPath="authors[0].lastName">
+                <TextControl
+                  labelProps={{
+                    width: '25%',
+                  }}
+                  label="Author (Last Name)"
+                  gridFlexWrap="nowrap"
+                  paddingVariant="tiny"
+                />
+              </ValueListItem>
+              <ValueListItem
+                textPath="citations[0].textValue"
+                rangesPath="citations[0].ranges"
+              >
+                <TextControl
+                  labelProps={{
+                    width: '25%',
+                  }}
+                  label="Citation"
+                  rich
+                  gridFlexWrap="nowrap"
+                  multiline
+                  paddingVariant="tiny"
+                />
+              </ValueListItem>
+            </ControlList>
+          </View>
+        </Grid>
+      </Section>
     </ValueListProvider>
-
-    // <ValueListProvider onChange={setValues} values={values}>
-    //   <List horizontalItemPadding="small" mb={20}>
-    //     <ValueListItem textPath="name" rangesPath="ranges">
-    //       <TextControl
-    //         rich
-    //         label="Name"
-    //         placeholder="descriptive name for the source"
-    //       />
-    //     </ValueListItem>
-    //     <ValueListItem textPath="authors[0].firstName">
-    //       <TextControl label="Author (first name)" />
-    //     </ValueListItem>
-    //     <ValueListItem textPath="authors[0].lastName">
-    //       <TextControl label="Author (last name)" />
-    //     </ValueListItem>
-    //     <ValueListItem
-    //       textPath="citations[0].textValue"
-    //       rangesPath="citations[0].ranges"
-    //     >
-    //       <TextControl label="Citation" rich />
-    //     </ValueListItem>
-    //   </List>
-    //   <View
-    //     id="formDocuments"
-    //     borderVariant="thinDark"
-    //     paddingVariant="none"
-    //     width="100%"
-    //     overflow="scroll"
-    //     maxWidth="500px"
-    //     flexShrink={1}
-    //   >
-    //     <pre>{JSON.stringify(values, null, 2)}</pre>
-    //   </View>
-    // </ValueListProvider>
   )
 }
 
