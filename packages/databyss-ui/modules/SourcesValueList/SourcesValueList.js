ae50cf5d02b688b8e4afc179647955304982d5cc
Michael <michaelsenejoa@gmail.com>
2019-12-19 11:04:51 -0500

    update versions

--- packages/databyss-ui/modules/SourcesValueList/SourcesValueList.js
+++ packages/databyss-ui/modules/SourcesValueList/SourcesValueList.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import ValueListProvider, {
   ValueListItem,
 } from '@databyss-org/ui/components/ValueList/ValueListProvider'
