b441fc75a75f657ff3982811dea602e7399e33ae
Michael <michaelsenejoa@gmail.com>
2019-12-27 16:49:04 -0500

    removed key from navigationprovder

--- packages/databyss-ui/components/Navigation/NavigationProvider/actions.js
+++ packages/databyss-ui/components/Navigation/NavigationProvider/actions.js
@@ -5,7 +5,7 @@ export function showModal({ component, props, dismiss }) {
     type: SHOW_MODAL,
     payload: {
       component,
-      props: { ...props, id: Date.now() },
+      props,
       dismiss,
     },
   }
