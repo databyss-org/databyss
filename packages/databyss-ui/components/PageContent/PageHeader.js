a88a368153d1505a7063071710b6ccc58feca0f7
Mila Sevilla <m.sevillazhou@gmail.com>
2020-05-03 14:15:58 +0200

    Code cleanup

--- packages/databyss-ui/components/PageContent/PageHeader.js
+++ packages/databyss-ui/components/PageContent/PageHeader.js
@@ -44,7 +44,6 @@ const PageHeader = ({ isFocused, pageId }) => {
     <View p="medium" flexGrow={1} ml="extraSmall">
       <TextInput
         onBlur={updatePageName}
-        autoFocus
         onFocus={() => isFocused(true)}
         onKeyDown={e => {
           if (e.key === 'Enter') {
