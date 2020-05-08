fa66b5e965cad1f49cb11f59a5a13762f44c81ce
Michael <michaelsenejoa@gmail.com>
2020-05-08 15:39:34 -0400

    start storybook

--- packages/databyss-ui/lib/saucelabs.js
+++ packages/databyss-ui/lib/saucelabs.js
@@ -1,16 +1,10 @@
 import webdriver, { Builder } from 'selenium-webdriver'
-import chrome from 'selenium-webdriver/chrome'
 
 export const CHROME = 'chrome'
 export const FIREFOX = 'firefox'
 export const SAFARI = 'safari'
 export const WIN = 'Windows 10'
 export const OSX = 'OS X 10.14'
-
-const screen = {
-  width: 640,
-  height: 480,
-}
 
 const username = process.env.SAUCE_USERNAME
 const accessKey = process.env.SAUCE_ACCESS_KEY
@@ -20,13 +14,7 @@ const accessKey = process.env.SAUCE_ACCESS_KEY
 export const startSession = process.env.LOCAL_ENV
   ? async () => {
       jest.setTimeout(800000)
-      const _builder = await new Builder()
-        .forBrowser(SAFARI)
-        // .setChromeOptions(
-        //   process.env.HEADLESS &&
-        //     new chrome.Options().headless().windowSize(screen)
-        // )
-        .build()
+      const _builder = await new Builder().forBrowser(SAFARI).build()
       return _builder
     }
   : async (name, platformName = OSX, browserName = SAFARI) => {
