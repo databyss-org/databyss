cb8bbce77e61e3e0153b48c2f3f65026823aa5ce
Michael <michaelsenejoa@gmail.com>
2020-05-13 15:04:13 -0400

    test fix

--- packages/databyss-editor/components/ContentEditable.js
+++ packages/databyss-editor/components/ContentEditable.js
@@ -1,13 +1,6 @@
 import React, { useMemo, useRef, useEffect } from 'react'
 import _ from 'lodash'
-import {
-  createEditor,
-  Node,
-  Transforms,
-  Point,
-  Range,
-  Editor as SlateEditor,
-} from 'slate'
+import { createEditor, Node, Transforms, Point, Range } from 'slate'
 import { withReact } from 'slate-react'
 import { produce } from 'immer'
 import { useSourceContext } from '@databyss-org/services/sources/SourceProvider'
