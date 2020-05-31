866fdbbaec762997343cf8596f201f2ffca78c76
pzhine <paul@hine.works>
2020-05-31 20:19:08 +0200

    try to get heroku review apps working

--- config/env.js
+++ config/env.js
@@ -30,12 +30,12 @@ const dotenvFiles = [
 // https://github.com/motdotla/dotenv-expand
 dotenvFiles.forEach(dotenvFile => {
   if (fs.existsSync(dotenvFile)) {
-    console.log('PROCESS ENV', dotenvFile, process.env.HEROKU_PR_NUMBER)
-    require('dotenv-expand')(
-      require('dotenv').config({
-        path: dotenvFile,
-      })
-    )
+    const _env = require('dotenv').config({
+      path: dotenvFile,
+    })
+    console.log('ENV', _env)
+    const _expandedEnv = require('dotenv-expand')(_env)
+    console.log('EXPANDED ENV', _expandedEnv)
   }
 })
 
