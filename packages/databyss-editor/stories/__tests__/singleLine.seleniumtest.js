fdab36bc49fe69eb9b4bc8da63864be2762f6af8
Michael <michaelsenejoa@gmail.com>
2020-06-05 12:38:42 -0400

    lint

--- packages/databyss-editor/stories/__tests__/singleLine.seleniumtest.js
+++ packages/databyss-editor/stories/__tests__/singleLine.seleniumtest.js
@@ -1,65 +1,66 @@
-/** @jsx h */
-/* eslint-disable func-names */
-import { By, Key } from 'selenium-webdriver'
-import assert from 'assert'
-import { startSession, OSX, SAFARI } from '@databyss-org/ui/lib/saucelabs'
-import { jsx as h } from './hyperscript'
-import { sanitizeEditorChildren } from './__helpers'
-import {
-  getEditor,
-  sleep,
-  getElementById,
-  getElementByTag,
-} from './_helpers.selenium'
+// TODO: TEST FOR SINGLE LINE EDITOR
+// /** @jsx h */
+// /* eslint-disable func-names */
+// import { By, Key } from 'selenium-webdriver'
+// import assert from 'assert'
+// import { startSession, OSX, SAFARI } from '@databyss-org/ui/lib/saucelabs'
+// import { jsx as h } from './hyperscript'
+// import { sanitizeEditorChildren } from './__helpers'
+// import {
+//   getEditor,
+//   sleep,
+//   getElementById,
+//   getElementByTag,
+// } from './_helpers.selenium'
 
-let driver
-let actions
-let editor
-let formDocuments
-
+// let driver
 // let actions
-const LOCAL_URL =
-  'http://localhost:6006/iframe.html?id=cypress-tests--valuelist-controller'
-const PROXY_URL =
-  'http://0.0.0.0:8080/iframe.html?id=cypress-tests--valuelist-controller'
+// let editor
+// let formDocuments
 
-export const CONTROL = process.env.LOCAL_ENV ? Key.META : Key.CONTROL
+// // let actions
+// const LOCAL_URL =
+//   'http://localhost:6006/iframe.html?id=cypress-tests--valuelist-controller'
+// const PROXY_URL =
+//   'http://0.0.0.0:8080/iframe.html?id=cypress-tests--valuelist-controller'
 
-describe('value list controller', () => {
-  beforeEach(async done => {
-    // OSX and safari are necessary
-    driver = await startSession('Slate-5-basic-osx-safari', OSX, SAFARI)
-    await driver.get(process.env.LOCAL_ENV ? LOCAL_URL : PROXY_URL)
+// export const CONTROL = process.env.LOCAL_ENV ? Key.META : Key.CONTROL
 
-    // formDocuments = await getElementById(driver, 'formDocuments')
-    actions = driver.actions()
-    done()
-  })
+// describe('value list controller', () => {
+//   beforeEach(async done => {
+//     // OSX and safari are necessary
+//     driver = await startSession('Slate-5-basic-osx-safari', OSX, SAFARI)
+//     await driver.get(process.env.LOCAL_ENV ? LOCAL_URL : PROXY_URL)
 
-  afterEach(async () => {
-    await driver.quit()
-  })
-  it('should accept inputs', async () => {
-    // name = await getElementByTag(driver, '[data-test-path="text"]')
+//     // formDocuments = await getElementById(driver, 'formDocuments')
+//     actions = driver.actions()
+//     done()
+//   })
 
-    let name = await getElementByTag(driver, '[data-test-path="text"]')
+//   afterEach(async () => {
+//     await driver.quit()
+//   })
+//   it('should accept inputs', async () => {
+//     // name = await getElementByTag(driver, '[data-test-path="text"]')
 
-    await name.click()
-    await actions.sendKeys('\t')
-    await actions.sendKeys('Name of text')
-    await actions.sendKeys('\t')
+//     let name = await getElementByTag(driver, '[data-test-path="text"]')
 
-    await actions.sendKeys('First name of source')
-    await actions.sendKeys('\t')
+//     await name.click()
+//     await actions.sendKeys('\t')
+//     await actions.sendKeys('Name of text')
+//     await actions.sendKeys('\t')
 
-    await actions.sendKeys('Last name of source')
-    await actions.sendKeys('\t')
-    await actions.sendKeys('citations of source')
+//     await actions.sendKeys('First name of source')
+//     await actions.sendKeys('\t')
 
-    await actions.perform()
+//     await actions.sendKeys('Last name of source')
+//     await actions.sendKeys('\t')
+//     await actions.sendKeys('citations of source')
 
-    // await name.sendKeys('first name')
-    // TODO: CHECK VALUES
-    assert.equal(true, true)
-  })
-})
+//     await actions.perform()
+
+//     // await name.sendKeys('first name')
+//     // TODO: CHECK VALUES
+//     assert.equal(true, true)
+//   })
+// })
