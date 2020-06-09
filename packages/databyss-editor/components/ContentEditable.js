95d3de4c296d80d11ba9ca2cb426367ffff5faf4
Michael <michaelsenejoa@gmail.com>
2020-06-09 18:02:27 -0400

    em dash

--- packages/databyss-editor/components/ContentEditable.js
+++ packages/databyss-editor/components/ContentEditable.js
@@ -4,6 +4,7 @@ import {
   Node,
   Transforms,
   Point,
+  Editor as SlateEditor,
   Range,
 } from '@databyss-org/slate'
 import { ReactEditor, withReact } from 'slate-react'
@@ -131,6 +132,31 @@ const ContentEditable = ({
       toggleMark(editor, 'location')
       event.preventDefault()
       return
+    }
+
+    if (event.key === '-') {
+      // check for m-dash
+      const _offset = flattenOffset(editor, editor.selection.focus)
+      if (_offset > 0) {
+        Transforms.move(editor, { distance: 1, edge: 'anchor', reverse: true })
+        const _frag = SlateEditor.fragment(editor, editor.selection)
+        if (Node.string(_frag[0]) === '-') {
+          // replace text with em dash
+          Transforms.delete(editor, {
+            distance: 1,
+            unit: 'character',
+            reverse: true,
+          })
+          Transforms.insertText(editor, '\u2014')
+          Transforms.move(editor, {
+            distance: 1,
+            edge: 'anchor',
+            reverse: true,
+          })
+          event.preventDefault()
+        }
+        Transforms.move(editor, { distance: 1, edge: 'anchor' })
+      }
     }
 
     if (event.key === 'Enter') {
