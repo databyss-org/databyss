0038789c96a4ed53ab27d8b8c11ece7b6daf24ea
msevilla <m.sevillazhou@gmail.com>
2020-06-16 19:32:44 +0200

    Sorting sources entries alphabetically and styling

--- packages/databyss-ui/components/SourcesContent/SourcesContent.js
+++ packages/databyss-ui/components/SourcesContent/SourcesContent.js
@@ -1,19 +1,13 @@
-import React, { useEffect, useMemo } from 'react'
-import Highlighter from 'react-highlight-words'
+import React, { useEffect } from 'react'
 import { useParams, Router } from '@reach/router'
 import { EntrySearchLoader } from '@databyss-org/ui/components/Loaders'
-import { useNavigationContext } from '@databyss-org/ui/components/Navigation/NavigationProvider/NavigationProvider'
 import {
   Text,
   View,
   BaseControl,
-  Grid,
-  Icon,
   ScrollView,
 } from '@databyss-org/ui/primitives'
-import PageSvg from '@databyss-org/ui/assets/page.svg'
 import { useSourceContext } from '@databyss-org/services/sources/SourceProvider'
-import { usePageContext } from '@databyss-org/services/pages/PageProvider'
 
 export const SourcesRouter = () => (
   <Router>
@@ -22,70 +16,43 @@ export const SourcesRouter = () => (
 )
 
 const SourcesContent = () => {
-  const { navigate } = useNavigationContext()
-  const { getPage } = usePageContext()
+  // const { query } = useParams()
   const { getAllSources, state } = useSourceContext()
-
   useEffect(() => getAllSources(), [])
-
-  // const onEntryClick = (pageId, blockId) => {
-  //   navigate(`/pages/${pageId}#${blockId}`)
-  // }
-
-  const onPageClick = pageId => {
-    navigate(`/pages/${pageId}`)
-  }
 
   const sourcesData = () =>
     Object.entries(state.cache).map(([, value]) => {
       const author = value.authors[0]
-      const firstName = author ? author.firstName.textValue : ''
-      const lastName = author ? author.lastName.textValue : ''
 
       return {
-        // pageId: value.account,
+        id: value._id,
         text: value.text.textValue,
-        author: `${firstName}, ${lastName}`,
+        author: {
+          firstName: author ? author.firstName.textValue : '',
+          lastName: author ? author.lastName.textValue : '',
+        },
       }
     })
 
-  // <SearchSourceLoader query={sourceQuery}>
-  //                 {results => {
-  //                   setSourcesLoaded(true)
-  //                   return (
-  //                     <ComposeResults
-  //                       results={results}
-  //                       onClick={onClick}
-  //                       unmount={() => setSourcesLoaded(false)}
-  //                     />
-  //                   )
-  //                 }}
-  //               </SearchSourceLoader>
+  const ComposeResults = () => {
+    const sortedSources = sourcesData().sort(
+      (a, b) => (a.text > b.text ? 1 : -1)
+    )
 
-  const ComposeResults = () =>
-    sourcesData().map((entry, index) => (
+    return sortedSources.map((entry, index) => (
       <View key={index} mb="medium">
-        <View height="40px">
-          <BaseControl hoverColor="background.2" activeColor="background.3">
-            <Grid singleRow alignItems="center" columnGap="small">
-              <Icon sizeVariant="small" color="text.3">
-                <PageSvg />
-              </Icon>
-              <Text variant="bodyLarge">{}</Text>
-            </Grid>
-          </BaseControl>
-        </View>
         <BaseControl
+          py="small"
+          px="small"
+          mx="em"
           hoverColor="background.2"
           activeColor="background.3"
-          onClick={() => onPageClick()}
         >
-          <View p="small" ml="small">
-            <Text>{entry.text}</Text>
-          </View>
+          <Text variant="bodyNormalSemibold">{entry.text}</Text>
         </BaseControl>
       </View>
     ))
+  }
 
   return (
     <ScrollView p="medium" flex="1" maxHeight="98vh">
@@ -94,7 +61,7 @@ const SourcesContent = () => {
           All Sources
         </Text>
       </View>
-      <EntrySearchLoader query="allSources">{ComposeResults}</EntrySearchLoader>
+      <EntrySearchLoader>{ComposeResults}</EntrySearchLoader>
     </ScrollView>
   )
 }
