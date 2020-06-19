e121ac041eb63ea31baf9615d3cb3116bfdfa960
msevilla <m.sevillazhou@gmail.com>
2020-06-19 17:23:58 +0200

    Allow more customizations for dropdown menu

--- packages/databyss-editor/components/DropdownMenu.js
+++ packages/databyss-editor/components/DropdownMenu.js
@@ -2,27 +2,36 @@ import React from 'react'
 import styledCss from '@styled-system/css'
 import theme, { borderRadius } from '@databyss-org/ui/theming/theme'
 import { pxUnits } from '@databyss-org/ui/theming/views'
-import { View, List } from '@databyss-org/ui/primitives'
+import { View } from '@databyss-org/ui/primitives'
 
-const _css = position => ({
-  padding: 'small',
+const _css = (position, open) => ({
   backgroundColor: 'background.0',
   zIndex: 1,
   position: 'absolute',
+  opacity: open ? 1 : 0,
   transition: `${theme.timing.quick}ms ease`,
   borderRadius,
   ...position,
 })
 
-const DropdownMenu = ({ children, position = { top: 0, left: 0 } }) => (
+const DropdownMenu = ({
+  children,
+  position = { top: 0, left: 0 },
+  minWidth = pxUnits(200),
+  minHeight = pxUnits(32),
+  maxWidth = pxUnits(500),
+  open,
+  ...others
+}) => (
   <View
-    // maxWidth="500px"
-    minWidth="200px"
-    // minHeight="32px"
+    maxWidth={maxWidth}
+    minWidth={minWidth}
+    minHeight={minHeight}
     shadowVariant="modal"
-    css={styledCss(_css({ top: position.top, left: position.left }))}
+    css={styledCss(_css({ top: position.top, left: position.left }, open))}
+    {...others}
   >
-    <List>{children}</List>
+    {children}
   </View>
 )
 
