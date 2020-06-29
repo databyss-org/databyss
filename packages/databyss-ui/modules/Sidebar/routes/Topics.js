cc873dc554be917fff85a85444f3244097e27d9e
Mila Sevilla Zhou <m.sevillazhou@gmail.com>
2020-06-29 17:49:04 +0200

    Showing list of topics in sidebar

--- packages/databyss-ui/modules/Sidebar/routes/Topics.js
+++ packages/databyss-ui/modules/Sidebar/routes/Topics.js
@@ -1,13 +1,32 @@
 import React from 'react'
 import SidebarList from '../../../components/Sidebar/SidebarList'
+import { AllTopicsLoader } from '@databyss-org/ui/components/Loaders'
+import { useTopicContext } from '@databyss-org/services/topics/TopicProvider'
 
-const menuItems = [
+const topicsOverview = [
   {
     type: 'topics',
     text: 'All Topics',
   },
 ]
 
-const Topics = () => <SidebarList menuItems={menuItems} />
+const Topics = () => {
+  const { state } = useTopicContext()
+
+  const topicsData = () =>
+    Object.entries(state.cache).map(([, value]) => ({
+      id: value._id,
+      type: 'topics',
+      text: value.text.textValue,
+    }))
+
+  const sortedSources = topicsData().sort((a, b) => (a.text > b.text ? 1 : -1))
+
+  return (
+    <AllTopicsLoader>
+      {() => <SidebarList menuItems={[...topicsOverview, ...sortedSources]} />}
+    </AllTopicsLoader>
+  )
+}
 
 export default Topics
