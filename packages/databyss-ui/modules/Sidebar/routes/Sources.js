3b4d4b7961cfb0c75e7aa46e5fb2036f6a1aa1ba
Mila Sevilla Zhou <m.sevillazhou@gmail.com>
2020-06-30 09:30:22 +0200

    Have list of individual authors in sidebar

--- packages/databyss-ui/modules/Sidebar/routes/Sources.js
+++ packages/databyss-ui/modules/Sidebar/routes/Sources.js
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useSourceContext } from '@databyss-org/services/sources/SourceProvider'
+import { EntrySearchLoader } from '@databyss-org/ui/components/Loaders'
+import AuthorSvg from '@databyss-org/ui/assets/author.svg'
 import SidebarList from '../../../components/Sidebar/SidebarList'
 
-const menuItems = [
+const sourcesOverview = [
   {
     type: 'sources',
     text: 'All sources',
@@ -12,6 +15,27 @@ const menuItems = [
   },
 ]
 
-const Sources = () => <SidebarList menuItems={menuItems} />
+const Sources = () => {
+  const { getAllSources, state } = useSourceContext()
+  useEffect(() => getAllSources(), [])
+
+  const sourcesData = () =>
+    Object.entries(state.cache).map(([, value]) => {
+      const author = value.authors[0]
+
+      return {
+        id: value._id,
+        type: 'authors',
+        text: author
+          ? `${author.lastName.textValue}, ${author.firstName.textValue}`
+          : 'Unknown author',
+        icon: <AuthorSvg />,
+      }
+    })
+
+  const sortedSources = sourcesData().sort((a, b) => (a.text > b.text ? 1 : -1))
+
+  return <SidebarList menuItems={[...sourcesOverview, ...sortedSources]} />
+}
 
 export default Sources
