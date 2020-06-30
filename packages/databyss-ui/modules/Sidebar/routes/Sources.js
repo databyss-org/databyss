0c94b27577ddee5b1f991a88df3ceb10aa9a4991
Mila Sevilla Zhou <m.sevillazhou@gmail.com>
2020-06-30 10:50:07 +0200

    Remove unknown authors

--- packages/databyss-ui/modules/Sidebar/routes/Sources.js
+++ packages/databyss-ui/modules/Sidebar/routes/Sources.js
@@ -1,16 +1,11 @@
 import React, { useEffect } from 'react'
 import { useSourceContext } from '@databyss-org/services/sources/SourceProvider'
-<<<<<<< HEAD
 import AuthorSvg from '@databyss-org/ui/assets/author.svg'
 import {
   getSourcesData,
   sortEntriesAtoZ,
   filterEntries,
 } from '@databyss-org/services/sources/util'
-=======
-import { EntrySearchLoader } from '@databyss-org/ui/components/Loaders'
-import AuthorSvg from '@databyss-org/ui/assets/author.svg'
->>>>>>> Have list of individual authors in sidebar
 import SidebarList from '../../../components/Sidebar/SidebarList'
 
 const sourcesOverview = [
