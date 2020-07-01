857852e678bb8d2ce04aa0296c92d092ade1f0d9
Mila Sevilla Zhou <m.sevillazhou@gmail.com>
2020-07-01 17:11:54 +0200

    Filtering for topics, sources & page titles

--- packages/databyss-ui/modules/Sidebar/routes/Pages.js
+++ packages/databyss-ui/modules/Sidebar/routes/Pages.js
@@ -2,20 +2,32 @@ import React from 'react'
 import { PagesLoader } from '@databyss-org/ui/components/Loaders'
 import SidebarList from '../../../components/Sidebar/SidebarList'
 
-const Pages = () => (
-  <PagesLoader>
-    {pages => {
-      const _menuItems = Object.values(pages).map(p => ({
-        text: p.name,
-        type: 'pages',
-        id: p._id,
-      }))
-      // alphabetize list
-      _menuItems.sort((a, b) => (a.text > b.text ? 1 : -1))
+const Pages = ({ filterQuery }) => (
+  <>
+    <PagesLoader>
+      {pages => {
+        const _menuItems = Object.values(pages).map(p => ({
+          text: p.name,
+          type: 'pages',
+          id: p._id,
+        }))
+        // alphabetize list
+        _menuItems.sort((a, b) => (a.text > b.text ? 1 : -1))
 
-      return <SidebarList menuItems={_menuItems} />
-    }}
-  </PagesLoader>
+        const filteredEntries = _menuItems.filter(entry =>
+          entry.text?.toLowerCase().includes(filterQuery.textValue)
+        )
+
+        return (
+          <SidebarList
+            menuItems={
+              filterQuery.textValue === '' ? _menuItems : filteredEntries
+            }
+          />
+        )
+      }}
+    </PagesLoader>
+  </>
 )
 
 export default Pages
