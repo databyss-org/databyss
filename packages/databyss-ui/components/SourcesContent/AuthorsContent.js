b91335dd64bfe0c2712e58e782b9b298f7946341
Mila Sevilla Zhou <m.sevillazhou@gmail.com>
2020-07-03 10:42:34 +0200

    Cleanup

--- packages/databyss-ui/components/SourcesContent/AuthorsContent.js
+++ packages/databyss-ui/components/SourcesContent/AuthorsContent.js
@@ -5,10 +5,6 @@ import {
   getSourcesData,
   sortEntriesAtoZ,
 } from '@databyss-org/services/sources/util'
-<<<<<<< HEAD
-=======
-import { groupBy, uniqBy } from 'lodash'
->>>>>>> Helper function or getting and sorting entry data
 import IndexPageEntries from '../PageContent/IndexPageEntries'
 import IndexPageContent from '../PageContent/IndexPageContent'
 
