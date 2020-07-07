e953441347021818cac9fff7d1a6c11b37660cc7
Michael <michaelsenejoa@gmail.com>
2020-07-06 21:24:46 -0400

    tests

--- packages/databyss-editor/components/ContentEditable.js
+++ packages/databyss-editor/components/ContentEditable.js
@@ -1,5 +1,11 @@
 import React, { useMemo, useRef, useEffect } from 'react'
-import { createEditor, Node, Transforms, Point } from '@databyss-org/slate'
+import {
+  createEditor,
+  Node,
+  Transforms,
+  Point,
+  Editor as SlateEditor,
+} from '@databyss-org/slate'
 import { ReactEditor, withReact } from 'slate-react'
 import _ from 'lodash'
 import { useSourceContext } from '@databyss-org/services/sources/SourceProvider'
@@ -171,6 +177,11 @@ const ContentEditable = ({
         editor.selection.focus.offset === 0
       const _doubleLineBreak = _nextIsBreak || _prevIsBreak || _atBlockStart
       if (!_doubleLineBreak && !symbolToAtomicType(_text.charAt(0))) {
+        // clear active marks
+        Object.keys(SlateEditor.marks(editor)).forEach(m =>
+          editor.removeMark(m)
+        )
+
         // we're not creating a new block, so just insert a carriage return
         event.preventDefault()
         Transforms.insertText(editor, `\n`)
