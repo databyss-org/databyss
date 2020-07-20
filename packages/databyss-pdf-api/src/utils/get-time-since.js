18acabd171f3d8b27340723d1be3b5007313a34d
Mat Janson Blanchet <mat.jb@jansensan.net>
2020-07-20 09:28:51 -0400

    Added service to parse PDF annotations

---
+++ packages/databyss-pdf-api/src/utils/get-time-since.js
@@ -0,0 +1,8 @@
+module.exports = startTime => {
+  if (typeof startTime !== 'number') {
+    throw new Error('Method expects `startTime` to be a number.')
+  }
+
+  const endTime = new Date().getTime()
+  return endTime - startTime
+}
