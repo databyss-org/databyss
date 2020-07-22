b5d776674cd30c2af8523dd9ba45af0d2c54c61d
Mila Sevilla Zhou <m.sevillazhou@gmail.com>
2020-07-22 19:51:18 +0200

    Wrap authorsLoader in CitationsLoader

--- packages/databyss-ui/modules/Sidebar/routes/Sources.js
+++ packages/databyss-ui/modules/Sidebar/routes/Sources.js
@@ -5,7 +5,10 @@ import {
   filterEntries,
   createSidebarListItems,
 } from '@databyss-org/services/entries/util'
-import { AuthorsLoader } from '@databyss-org/ui/components/Loaders'
+import {
+  AuthorsLoader,
+  SourceCitationsLoader,
+} from '@databyss-org/ui/components/Loaders'
 import SidebarList from '@databyss-org/ui/components/Sidebar/SidebarList'
 
 const sourcesOverview = [
@@ -22,47 +25,53 @@ const sourcesOverview = [
 ]
 
 const Sources = ({ filterQuery }) => (
-  <AuthorsLoader>
-    {authors => {
-      const authorData = Object.values(authors).map(value => {
-        const firstName = value.firstName?.textValue
-        const shortFirstName = `${firstName?.charAt(0)}.`
-        const lastName = value.lastName?.textValue
+  <SourceCitationsLoader>
+    {() => (
+      <AuthorsLoader>
+        {authors => {
+          const authorData = Object.values(authors).map(value => {
+            const firstName = value.firstName?.textValue
+            const shortFirstName = `${firstName?.charAt(0)}.`
+            const lastName = value.lastName?.textValue
 
-        const getShortAuthorName = () => {
-          if (lastName && firstName) {
-            return `${lastName}, ${shortFirstName}`
-          }
-          return lastName || shortFirstName
-        }
+            const getShortAuthorName = () => {
+              if (lastName && firstName) {
+                return `${lastName}, ${shortFirstName}`
+              }
+              return lastName || shortFirstName
+            }
 
-        const authorParams = new URLSearchParams({
-          author_first: encodeURIComponent(firstName),
-          author_last: encodeURIComponent(lastName),
-        })
+            const authorParams = new URLSearchParams({
+              author_first: encodeURIComponent(firstName),
+              author_last: encodeURIComponent(lastName),
+            })
 
-        return createSidebarListItems({
-          text: getShortAuthorName(),
-          type: 'authors',
-          route: '/sources',
-          params: authorParams.toString(),
-          icon: <AuthorSvg />,
-        })
-      })
+            return createSidebarListItems({
+              text: getShortAuthorName(),
+              type: 'authors',
+              route: '/sources',
+              params: authorParams.toString(),
+              icon: <AuthorSvg />,
+            })
+          })
 
-      const sortedAuthors = sortEntriesAtoZ(authorData, 'text')
-      const filteredEntries = filterEntries(sortedAuthors, filterQuery)
+          const sortedAuthors = sortEntriesAtoZ(authorData, 'text')
+          const filteredEntries = filterEntries(sortedAuthors, filterQuery)
 
-      return (
-        <SidebarList
-          menuItems={[
-            ...sourcesOverview,
-            ...(filterQuery.textValue === '' ? sortedAuthors : filteredEntries),
-          ]}
-        />
-      )
-    }}
-  </AuthorsLoader>
+          return (
+            <SidebarList
+              menuItems={[
+                ...sourcesOverview,
+                ...(filterQuery.textValue === ''
+                  ? sortedAuthors
+                  : filteredEntries),
+              ]}
+            />
+          )
+        }}
+      </AuthorsLoader>
+    )}
+  </SourceCitationsLoader>
 )
 
 export default Sources
