4de01c5d6dac9c6db46f1f4ee1eb47527d82426b
Mila Sevilla Zhou <m.sevillazhou@gmail.com>
2020-07-25 18:09:36 +0200

    Add react-responsive library for media queries

--- packages/databyss-ui/modules/Homepage/SignUpForm.js
+++ packages/databyss-ui/modules/Homepage/SignUpForm.js
@@ -3,28 +3,38 @@ import { View, TextInput, Button } from '@databyss-org/ui/primitives'
 import { pxUnits } from '@databyss-org/ui/theming/views'
 import { useMediaQuery } from 'react-responsive'
 
-const SignUpForm = () => (
-  // const isMobile = useMediaQuery({ maxWidth: 600 })
+const SignUpForm = () => {
+  const isMobile = useMediaQuery({ maxWidth: 425 })
 
-  <View flexDirection="row" justifyContent="center" mb={pxUnits(48)}>
+  return (
     <View
-      backgroundColor="background.2"
+      flexDirection={isMobile ? 'column' : 'row'}
       justifyContent="center"
-      py="small"
-      px="em"
-      borderRadius="default"
-      mr="em"
-      width="100%"
-      maxWidth={pxUnits(240)}
+      alignItems="center"
+      mb={pxUnits(48)}
     >
-      <TextInput
-        placeholder="Enter your email..."
-        value="email"
-        variant="uiTextNormal"
-      />
+      <View
+        backgroundColor="background.2"
+        justifyContent="center"
+        py="small"
+        px="em"
+        borderRadius="default"
+        mr={isMobile ? 'none' : 'em'}
+        mb={isMobile ? 'em' : 'none'}
+        width="100%"
+        maxWidth={isMobile ? 'none' : pxUnits(240)}
+      >
+        <TextInput
+          placeholder="Enter your email..."
+          value="email"
+          variant="uiTextNormal"
+        />
+      </View>
+      <Button variant="pinkHighlighted" width={isMobile ? '100%' : 'auto'}>
+        Sign up
+      </Button>
     </View>
-    <Button variant="pinkHighlighted">Sign up</Button>
-  </View>
-)
+  )
+}
 
 export default SignUpForm
