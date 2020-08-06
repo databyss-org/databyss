bd17b560a4beb5ecc081cccc377a788599fc13e2
Mat Janson Blanchet <mat.jb@jansensan.net>
2020-08-06 11:06:13 -0400

    Converted to use request()
    Signed-off-by: Mat Janson Blanchet <mat.jb@jansensan.net>

--- packages/databyss-services/pdf/index.ts
+++ packages/databyss-services/pdf/index.ts
@@ -1,26 +1,17 @@
-/* eslint-disable prefer-promise-reject-errors */
+import request from '../lib/request'
 
 const URL = `${process.env.PDF_API_URL}/pdf/parse`
 
-export const fetchAnnotations = (file: File): Promise<any> =>
-  new Promise((resolve, reject) => {
-    const xhr = new XMLHttpRequest()
+export const fetchAnnotations = (file: File): Promise<any> => {
+  const formData = new FormData()
+  formData.append('pdf', file)
 
-    xhr.onload = () => {
-      if (xhr.status === 200) {
-        resolve(JSON.parse(xhr.response))
-      } else {
-        reject({ message: xhr.response })
-      }
-    }
-
-    xhr.onerror = () => {
-      reject({ message: xhr.response })
-    }
-
-    const formData = new FormData()
-    formData.append('pdf', file)
-
-    xhr.open('POST', URL)
-    xhr.send(formData)
-  })
+  return request(
+    URL,
+    {
+      method: 'POST',
+      body: formData,
+    },
+    false
+  )
+}
