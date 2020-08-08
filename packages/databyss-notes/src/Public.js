bcf81729fd972310dbbdb8c1bdc84c820c373b4a
Mila Sevilla Zhou <m.sevillazhou@gmail.com>
2020-08-08 12:14:37 +0200

    Remove routing from homepage

--- packages/databyss-notes/src/Public.js
+++ packages/databyss-notes/src/Public.js
@@ -1,11 +1,6 @@
 import React from 'react'
 import Homepage from '@databyss-org/ui/modules/Homepage/Homepage'
-import { Router } from '@reach/router'
 
-const Public = () => (
-  <Router style={{ width: '100%' }}>
-    <Homepage path="/" />
-  </Router>
-)
+const Public = () => <Homepage />
 
 export default Public
