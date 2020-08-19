674bc7d1542a52c3345ac1fd754f534c50ec9653
Michael <michaelsenejoa@gmail.com>
2020-08-19 17:01:40 -0400

    block indexing test

---
+++ packages/databyss-editor/stories/__tests__/blockIndexing.seleniumtest.js
@@ -0,0 +1,145 @@
+/* eslint-disable func-names */
+import { Key, By } from 'selenium-webdriver'
+import assert from 'assert'
+import { startSession, OSX, CHROME } from '@databyss-org/ui/lib/saucelabs'
+import {
+  getElementByTag,
+  sleep,
+  sendKeys,
+  enterKey,
+  rightShiftKey,
+} from './_helpers.selenium'
+
+let driver
+let actions
+const LOCAL_URL = 'http://localhost:3000'
+const PROXY_URL = 'http://0.0.0.0:3000'
+
+export const CONTROL = process.env.LOCAL_ENV ? Key.META : Key.CONTROL
+
+describe('block indexing', () => {
+  beforeEach(async done => {
+    const random = Math.random()
+      .toString(36)
+      .substring(7)
+    // OSX and chrome are necessary
+    driver = await startSession({ platformName: OSX, browserName: CHROME })
+    await driver.get(process.env.LOCAL_ENV ? LOCAL_URL : PROXY_URL)
+
+    const emailField = await getElementByTag(driver, '[data-test-path="email"]')
+    await emailField.sendKeys(`${random}@test.com`)
+
+    let continueButton = await getElementByTag(
+      driver,
+      '[data-test-id="continueButton"]'
+    )
+    await continueButton.click()
+
+    const codeField = await getElementByTag(driver, '[data-test-path="code"]')
+    await codeField.sendKeys('test-code-42')
+
+    continueButton = await getElementByTag(
+      driver,
+      '[data-test-id="continueButton"]'
+    )
+
+    await continueButton.click()
+
+    await sleep(1000)
+
+    actions = driver.actions()
+
+    done()
+  })
+
+  afterEach(async () => {
+    await driver.quit()
+  })
+
+  //   3)  Tests for indexing [adding a topic, adds it to the index, clicking on it should show results, clicking on results should show page with correct entries]
+  // 4) Check for indexing closure accuracy [delete an atomic, it should remove all entries from that atomic; if you add an end atomic, it should only show entries up until that end atomic; if you delete an end atomic, it should carry on until there's another atomic] 5) Click on all authors brings up all authors, clicking on all sources brings up all sources, clicking on all topics brings up all topics 6) Clicking on an author in all authors, should bring you to that author's entry result page (same for all sources, and all topics)
+
+  it('should test block indexing', async () => {
+    // populate a page
+    await sleep(500)
+    await sendKeys(actions, 'this is the first page title')
+    await enterKey(actions)
+
+    await sendKeys(actions, '@this is an opening source')
+    await enterKey(actions)
+    await sendKeys(actions, 'this is an entry')
+    await enterKey(actions)
+    await enterKey(actions)
+    await sendKeys(actions, '#a topic')
+    await enterKey(actions)
+    await sendKeys(actions, 'second entry')
+    await enterKey(actions)
+    await enterKey(actions)
+    await sendKeys(actions, 'third entry')
+    await enterKey(actions)
+    await enterKey(actions)
+    await sendKeys(actions, '/#')
+    await enterKey(actions)
+    await sendKeys(actions, 'fourth entry not in atomic')
+    await enterKey(actions)
+    await enterKey(actions)
+    await sendKeys(actions, '#this is the second topic')
+    await enterKey(actions)
+    await sendKeys(actions, 'entry should be contained within topic')
+    await enterKey(actions)
+    await enterKey(actions)
+    await sendKeys(actions, 'second entry within topic')
+    await sleep(2000)
+
+    await driver.navigate().refresh()
+    await sleep(2000)
+
+    const topicsSidebarButton = await getElementByTag(
+      driver,
+      '[data-test-sidebar-element="topics"]'
+    )
+    await topicsSidebarButton.click()
+
+    // assure two topics are show in sidebar
+    const secondTopic = await getElementByTag(
+      driver,
+      '[data-test-element="page-sidebar-2"]'
+    )
+
+    await secondTopic.click()
+
+    await sleep(1000)
+
+    const topicResults = await driver.findElements(
+      By.tagName('[data-test-element="atomic-results"]')
+    )
+
+    await sleep(1000)
+
+    // check for that topic exists on page
+    assert.equal(topicResults.length, 1)
+
+    const topicEntries = await driver.findElements(
+      By.tagName('[data-test-element="atomic-result-item"]')
+    )
+
+    // assure two results are listed under entry
+    assert.equal(topicEntries.length, 2)
+
+    await topicEntries[1].click()
+    await sleep(1000)
+    await rightShiftKey(actions)
+    await rightShiftKey(actions)
+    await rightShiftKey(actions)
+    await rightShiftKey(actions)
+    await rightShiftKey(actions)
+    await rightShiftKey(actions)
+
+    // get highlighted text
+    const _selection = await driver.executeScript(
+      'return window.getSelection().toString()'
+    )
+    // check highlight for correct words
+    assert.equal(_selection, 'second')
+  })
+})
