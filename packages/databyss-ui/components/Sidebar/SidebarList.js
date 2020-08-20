26395dc343a10c0caf87ef3fb956a38209cc1958
Mila Sevilla Zhou <m.sevillazhou@gmail.com>
2020-08-20 16:45:15 +0200

    Fix active item in sidebar

--- packages/databyss-ui/components/Sidebar/SidebarList.js
+++ packages/databyss-ui/components/Sidebar/SidebarList.js
@@ -29,6 +29,22 @@ const SidebarList = ({ menuItems, query, height, ...others }) => {
     return `${item.route}`
   }
 
+  const getActiveItem = item => {
+    // For authors the url structure changes to query parameters separated by '?'
+    if (location.search) {
+      return `?${item.params}` === location.search
+    }
+    // For topics, pages, and search, the url is separated by id or search param with a '/'
+    if (item.params) {
+      return item.params === tokens.params
+    }
+    // For index pages
+    if (!location.search) {
+      return item.route === location.pathname
+    }
+    return false
+  }
+
   return (
     <View
       width="100%"
@@ -39,14 +55,10 @@ const SidebarList = ({ menuItems, query, height, ...others }) => {
       {...others}
     >
       {menuItems.map((item, index) => {
-        const _isActive = item.params
-          ? item.params === tokens.params
-          : item.route === location.pathname
-
         if (item.text) {
           return (
             <SidebarListItem
-              isActive={_isActive}
+              isActive={getActiveItem(item)}
               text={item.text}
               href={getHref(item)}
               key={`${item.type}-${index}`}
@@ -54,7 +66,7 @@ const SidebarList = ({ menuItems, query, height, ...others }) => {
               icon={
                 <Icon
                   sizeVariant="tiny"
-                  color={_isActive ? 'text.1' : 'text.3'}
+                  color={getActiveItem(item) ? 'text.1' : 'text.3'}
                   mt={pxUnits(2)}
                 >
                   {item.icon ? item.icon : menuSvgs(item.type)}
