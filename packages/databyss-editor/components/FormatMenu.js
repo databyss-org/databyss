55f279d3f0ff6778c49471a72ccd36df07707fcf
Mila Sevilla Zhou <m.sevillazhou@gmail.com>
2020-09-07 15:04:59 +0200

    Code cleanup

--- packages/databyss-editor/components/FormatMenu.js
+++ packages/databyss-editor/components/FormatMenu.js
@@ -3,13 +3,12 @@ import { Button, Text, View } from '@databyss-org/ui/primitives'
 import { useEditor, ReactEditor, useSlate } from '@databyss-org/slate-react'
 import { isMobileOs } from '@databyss-org/ui/'
 import { pxUnits } from '@databyss-org/ui/theming/views'
-import { Editor, Range } from '@databyss-org/slate'
+import { Range } from '@databyss-org/slate'
 import useEventListener from '@databyss-org/ui/lib/useEventListener'
 import HoveringToolbar from './HoveringToolbar'
 import {
   isFormatActive,
   toggleMark,
-  isSelectionAtomic,
   slateSelectionToStateSelection,
 } from './../lib/slateUtils'
 
@@ -126,29 +125,6 @@ const isBackwards = stateSelection => {
   }
   return stateSelection.anchor.index > stateSelection.focus.index
 }
-// const isBackwards = (domSelection, editor) => {
-//   const stateSelection = slateSelectionToStateSelection(editor)
-//   const [isBackwards, setIsBackwards] = useState(false)
-
-//   if (stateSelection.anchor.index === stateSelection.focus.index) {
-//     return setIsBackwards(domSelection.anchorOffset - domSelection.focusOffset > 0
-//   }
-
-//   return stateSelection.anchor.index > stateSelection.focus.index
-// }
-
-const getNumberOfEmptyBlocks = blocks => {
-  let countEmptyBlocks = 0
-
-  blocks.forEach(block => {
-    if (block.children[0].text === '') {
-      return (countEmptyBlocks += 1)
-    }
-    return 0
-  })
-
-  return countEmptyBlocks
-}
 
 const FormatMenu = () => {
   const ref = useRef()
@@ -170,57 +146,19 @@ const FormatMenu = () => {
     const _rects = domRange.getClientRects()
     const _length = _rects.length
 
-    // const nonEmptyRects = Object.entries(_rects)
-    //   .map(rect => (rect[1].width !== 0 ? rect : null))
-    //   .filter(Boolean)
-
-    // const _length = nonEmptyRects.length
-
     const blocks = editor.getFragment(selection)
-
-    const isBlockEmpty = block => block?.children[0].text === ''
 
     // get the correct position if you select an empty space
     const backwardsRect =
       blocks[0]?.children[0].text === '' && _rects.length > 1
         ? _rects[1]
         : _rects[0]
-    // const backwardsRect = () => {
-    //   const countEmptyBlocks = getNumberOfEmptyBlocks(blocks)
-    //   const firstBlockIsEmpty = isBlockEmpty(blocks[0])
 
-    //   if (countEmptyBlocks > 1 && firstBlockIsEmpty) {
-    //     return nonEmptyRects[countEmptyBlocks - 1]?.[1]
-    //   }
-
-    //   return nonEmptyRects[0]?.[1]
-    // }
-
-    // const forwardsRect = () => {
-    //   const countEmptyBlocks = getNumberOfEmptyBlocks(blocks)
-    //   const lastBlockIsEmpty = isBlockEmpty(blocks[_length - 1])
-
-    //   if (countEmptyBlocks > 1 && lastBlockIsEmpty) {
-    //     return nonEmptyRects[_length - (countEmptyBlocks + 1)]?.[1]
-    //   }
-
-    //   return nonEmptyRects[_length - 1]?.[1]
-    // }
-
-    // const getForwardsRectWidth = rect => {
-    //   if (nonEmptyRects[_length - 1].width === 0) {
-    //     return 0
-    //   }
-    //   return rect.width
-    // }
     const rect = isBackwards ? backwardsRect : _rects[_length - 1]
-
-    // const rect = isBackwards ? backwardsRect() : forwardsRect()
 
     return setPosition({
       top: pxUnits(rect.top - el?.clientHeight),
       left: pxUnits(rect.left + (isBackwards ? 0 : rect.width)),
-      // left: pxUnits(rect.left + (isBackwards ? 0 : getForwardsRectWidth(rect))),
     })
   }
 
@@ -244,8 +182,6 @@ const FormatMenu = () => {
         !selection ||
         !ReactEditor.isFocused(editor) ||
         Range.isCollapsed(selection) ||
-        // Editor.string(editor, selection) === '' ||
-        // isSelectionAtomic(editor)
         domSelection.isCollapsed === true
 
       if (dontShowMenu) {
@@ -258,16 +194,12 @@ const FormatMenu = () => {
   useEventListener('mouseup', () => {
     const domSelection = window.getSelection()
     const isTextSelected = domSelection.isCollapsed === false
-    // const domRange = domSelection.getRangeAt(0)
-    // get selected dom nodes
-    // const _rects = domRange.getClientRects()
 
     if (isTextSelected) {
       const __isBackwards = isSelectionBackwards
-      // if (_rects) {
+
       updatePosition(domSelection, __isBackwards)
       setMenuActive(true)
-      // }
     }
   })
 
