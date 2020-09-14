201b730097cdcf162f7079d7a42128d959039e59
Paul Hine <paul@hine.works>
2020-09-14 14:27:38 -0400

    update user in User.create if email already exists (#469)

--- packages/databyss-api/src/models/User.js
+++ packages/databyss-api/src/models/User.js
@@ -34,20 +34,27 @@ const UserSchema = new mongoose.Schema({
 
 UserSchema.static('create', async (values = {}) => {
   const User = mongoose.model('user', UserSchema)
-  const instance = User(values)
 
-  // save the user to generate an id
-  if (!values._id) {
+  // if email exists, update the User
+  let instance = await User.findOne({ email: values.email })
+  if (!instance) {
+    instance = new User()
+  }
+  Object.assign(instance, values)
+
+  // save the user here to generate an id
+  await instance.save()
+
+  // create default account if it doesn't exist
+  if (!instance.defaultAccount) {
+    const account = await Account.create()
+    instance.defaultAccount = account._id
+
+    // set user as ADMIN on account
+    await account.setUserRole({ userId: instance._id, role: 'ADMIN' })
     await instance.save()
   }
 
-  // create default account
-  const account = await Account.create()
-  instance.defaultAccount = account._id
-
-  // set user as ADMIN on account
-  await account.setUserRole({ userId: instance._id, role: 'ADMIN' })
-  await instance.save()
   return instance
 })
 
