408c443c606e12d6ee9691c7472de1454d76e2f2
Michael <michaelsenejoa@gmail.com>
2020-09-15 13:00:24 -0400

    links

--- packages/databyss-editor/components/Editor.js
+++ packages/databyss-editor/components/Editor.js
@@ -1,8 +1,9 @@
 import React, { useCallback } from 'react'
 import { Slate, Editable } from '@databyss-org/slate-react'
 import { useSessionContext } from '@databyss-org/services/session/SessionProvider'
-import { Text } from '@databyss-org/slate'
+import { Text, Node } from '@databyss-org/slate'
 import { useEntryContext } from '@databyss-org/services/entries/EntryProvider'
+import linksFinder from 'links-finder'
 import { useEditorContext } from '../state/EditorProvider'
 import Leaf from './Leaf'
 import Element from './Element'
@@ -44,6 +45,32 @@ const Editor = ({
   const decorate = useCallback(
     ([node, path]) => {
       const ranges = []
+
+      // check for url in text
+      const _string = Node.string(node)
+
+      linksFinder.wrapLinks(_string, {
+        onMatch: link => {
+          // split string by link
+          const _parts = _string.split(link)
+          if (_parts.length > 1) {
+            let offset = 0
+
+            // add url link to markup
+            _parts.forEach((part, i) => {
+              if (i !== 0) {
+                ranges.push({
+                  anchor: { path, offset: offset - link.length },
+                  focus: { path, offset },
+                  url: link,
+                })
+              }
+
+              offset = offset + part.length + link.length
+            })
+          }
+        },
+      })
 
       if (!searchTerm.length) {
         return ranges
--- packages/databyss-editor/components/Leaf.js
+++ packages/databyss-editor/components/Leaf.js
@@ -15,6 +15,31 @@ const Leaf = ({ attributes, children, leaf }) => {
     _children = <span style={{ color: '#A19A91' }}>{_children}</span>
   }
 
+  if (leaf.url) {
+    _children = (
+      <a
+        onClick={() => {
+          // validate url
+          let _url = leaf.url
+          if (
+            !(_url.indexOf('http://') === 0 || _url.indexOf('https://') === 0)
+          ) {
+            _url = `http://${_url}`
+            // do something here
+          }
+          window.open(_url, '_blank')
+        }}
+        href={leaf.url}
+        style={{
+          color: 'blue',
+          cursor: 'pointer',
+        }}
+      >
+        {_children}
+      </a>
+    )
+  }
+
   return (
     <span
       {...attributes}
