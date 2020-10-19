d0746a1287f64142bbe0c404e038bfa1a2e42800
pzhine <paul@hine.works>
2020-10-19 21:24:43 +0200

    - Rollback citation-js version to 0.5.0-alpha.6 to fix intermittent errors, as per https://github.com/citation-js/citation-js/issues/101 - Remove cruft from previous workaround attempts

--- packages/databyss-services/citations/actions.ts
+++ packages/databyss-services/citations/actions.ts
@@ -1,14 +1,8 @@
 import { SourceDetail } from '../interfaces'
-
-import {
-  CACHE_CITATION,
-  CITEPROC_ERROR,
-  PROCESS_CITATION,
-} from './constants/Actions'
+import { CACHE_CITATION, PROCESS_CITATION } from './constants/Actions'
 import { CitationFormatOptions } from '.'
 import { formatCitation } from './services/formatCitation'
 import { toJsonCsl } from './services/toJsonCsl'
-import CitationProcessStatus from './constants/CitationProcessStatus'
 
 export interface CitationDTO {
   source: SourceDetail
@@ -19,78 +13,25 @@ export interface CitationProcessOptions {
   hash: String
 }
 
-export function processCitation() {
-  console.info('--- actions.processCitation ---')
-
-  return async (dispatch: Function, getState: () => any) => {
-    const state = getState()
-    console.log('state.status:', state.status)
-
-    if (state.status === CitationProcessStatus.PROCESSING) {
-      console.warn('ignored...')
-      return
-    }
-
-    const { queue } = state
-    const { current } = queue
-    const { data, options } = current
-
-    console.log('start process')
+export function processCitation(data: CitationDTO, hash: String) {
+  return async (dispatch: Function) => {
+    console.info('--- actions.processCitation ---')
     dispatch({
       type: PROCESS_CITATION,
-      payload: { hash: options.hash },
+      payload: { hash },
     })
 
-    try {
-      const csl = toJsonCsl(data.source)
-      const citation = await formatCitation(csl, data.options)
+    const csl = toJsonCsl(data.source)
+    const citation = await formatCitation(csl, data.options)
 
-      // cache citation
-      console.log('obtained citation, cache it')
-      dispatch({
-        type: CACHE_CITATION,
-        payload: {
-          citation,
-          hash: options.hash,
-        },
-      })
-
-      if (queue.next && queue.next.hash !== queue.current.hash) {
-        // next differs from current, restart process
-        console.log('process next in queue')
-        queue.current = queue.next
-        queue.next = null
-        dispatch(processCitation())
-      } else {
-        // same hash, thus same data, clear queue
-        queue.current = null
-        queue.next = null
-        console.log('queue cleared!!')
-      }
-    } catch (error) {
-      console.warn('error:', error.message)
-
-      if (error.message === CITEPROC_ERROR) {
-        // This most likely occurs because citation library
-        // is not being defensive enough internally:
-        // In citeproc_commonjs.js,
-        // they do not check if `Item` exists
-        // before trying to get its props.
-        // ```
-        // CSL.Disambiguation.prototype.getCiteData
-        //   if (!this.maxNamesByItemId[Item.id]) { ...
-        // ```
-        dispatch({
-          type: CACHE_CITATION,
-          payload: {
-            citation: CITEPROC_ERROR,
-            hash: options.hash,
-          },
-        })
-        // FIXME: what now!?
-      } else {
-        throw error
-      }
-    }
+    // cache citation
+    console.log('obtained citation, cache it')
+    dispatch({
+      type: CACHE_CITATION,
+      payload: {
+        citation,
+        hash,
+      },
+    })
   }
 }
--- packages/databyss-services/citations/reducer.ts
+++ packages/databyss-services/citations/reducer.ts
@@ -3,41 +3,19 @@ import produce from 'immer'
 import { FSA } from '../interfaces'
 import { ResourcePending } from '../interfaces/ResourcePending'
 
-import CitationProcessStatus from './constants/CitationProcessStatus'
-import {
-  CACHE_CITATION,
-  CITEPROC_ERROR,
-  PROCESS_CITATION,
-  STOP_PROCESSING_CITATION,
-} from './constants/Actions'
+import { CACHE_CITATION, PROCESS_CITATION } from './constants/Actions'
 
 export default produce((state: any, action: FSA) => {
   switch (action.type) {
     case PROCESS_CITATION: {
       const { hash } = action.payload
-      state.status = CitationProcessStatus.PROCESSING
       state.citationCache[hash] = new ResourcePending()
       break
     }
 
     case CACHE_CITATION: {
       const { citation, hash } = action.payload
-      if (citation !== CITEPROC_ERROR) {
-        state.status = CitationProcessStatus.IDLE
-        state.citationCache[hash] = citation
-        state.errorCount = 0
-      } else {
-        state.status = CitationProcessStatus.ERROR
-        state.errorCount += 1
-      }
-      break
-    }
-
-    case STOP_PROCESSING_CITATION: {
-      state.status = CitationProcessStatus.IDLE
-      state.queue.current = null
-      state.queue.next = null
-      state.errorCount = 0
+      state.citationCache[hash] = citation
       break
     }
   }
