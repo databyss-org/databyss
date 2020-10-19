1e0879496bd3fc2682d119d2326a8af2574fc1fd
pzhine <paul@hine.works>
2020-10-19 21:24:43 +0200

    - Rollback citation-js version to 0.5.0-alpha.6 to fix intermittent errors, as per https://github.com/citation-js/citation-js/issues/101 - Remove cruft from previous workaround attempts

--- packages/databyss-services/citations/CitationProvider.tsx
+++ packages/databyss-services/citations/CitationProvider.tsx
@@ -2,14 +2,10 @@ import { createContext, useContextSelector } from 'use-context-selector'
 import { debounce } from 'lodash'
 import MurmurHash3 from 'imurmurhash'
 import React, { useCallback } from 'react'
-
 import createReducer from '@databyss-org/services/lib/createReducer'
-
 import { SourceDetail } from '../interfaces'
-
-import { CitationDTO, CitationProcessOptions, processCitation } from './actions'
+import { CitationDTO, processCitation } from './actions'
 import { CitationFormatOptions } from '.'
-import { CITEPROC_ERROR, STOP_PROCESSING_CITATION } from './constants/Actions'
 import CitationProcessStatus from './constants/CitationProcessStatus'
 import reducer from './reducer'
 
@@ -18,11 +14,13 @@ interface PropsType {
 }
 
 interface ContextType {
-  citationHTML: string
+  generateCitation: (
+    source: SourceDetail,
+    options: CitationFormatOptions
+  ) => String
 }
 
 const useReducer = createReducer()
-const maxErrorCount = 10
 
 export const CitationContext = createContext<ContextType | null>(null)
 export const citationUpdateCooldown = 1500
@@ -53,76 +51,24 @@ const CitationProvider: React.FunctionComponent<PropsType> = (
   )
 
   const debouncedProcessCitation = useCallback(
-    debounce((data: CitationDTO, options: CitationProcessOptions) => {
+    debounce((data: CitationDTO, hash: String) => {
       console.info('--- CitationProvider.debouncedProcessCitation ---')
-
-      const { queue, status, errorCount } = state
-
-      if (status === CitationProcessStatus.ERROR) {
-        console.warn('well thats your problem!')
-        console.log('error count', errorCount)
-
-        if (errorCount > maxErrorCount) {
-          console.warn('give up, too many errors...')
-          console.log('current cache:', state.citationCache[options.hash])
-          dispatch({ type: STOP_PROCESSING_CITATION })
-          return null
-        }
-
-        const isSameHash = queue.current.options.hash === options.hash
-        if (!isSameHash) {
-          console.warn('hash differs, update queue')
-          queue.current = { data, options }
-          queue.next = null
-        }
-
-        console.log('retry...')
-        dispatch(processCitation())
-        return null
-      }
-
-      // already cache of current hash
-      const currentCitationCache = state.citationCache[options.hash]
-      if (currentCitationCache && currentCitationCache !== CITEPROC_ERROR) {
-        console.warn('already has citation, returning')
-        console.log('currentCitationCache:', currentCitationCache)
-        dispatch({ type: STOP_PROCESSING_CITATION })
-        return currentCitationCache
-      }
-
-      // no cache or no current queue
-      if (!state.citationCache[options.hash] || !queue.current) {
-        console.warn('no cache or no current queue, start process')
-        queue.current = { data, options }
-        queue.next = null
-        dispatch(processCitation())
-        return null
-      }
-
-      return state.citationCache[options.hash]
-    }, citationUpdateCooldown)
+      dispatch(processCitation(data, hash))
+      return null
+    }, citationUpdateCooldown),
+    [state.citationCache]
   )
 
   const generateCitation = useCallback(
     (source: SourceDetail, options: CitationFormatOptions) => {
       console.info('--- CitationProvider.generateCitation ---')
 
-      const { status } = state
-
       const hash = generateHash(source, options)
-
-      // already in process
-      if (status === CitationProcessStatus.PROCESSING) {
-        console.warn('already processing, updating next in queue')
-        state.queue.next = {
-          data: { source, options },
-          options: { hash },
-        }
-        return null
+      if (state.citationCache[hash]) {
+        return state.citationCache[hash]
       }
 
-      return debouncedProcessCitation({ source, options }, { hash })
-      // return state.citationCache[hash]
+      return debouncedProcessCitation({ source, options }, hash)
     },
     [state]
   )
