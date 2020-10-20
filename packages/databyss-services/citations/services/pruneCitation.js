f68dc35ac0527bb62cedfd8ce7cb0f5bf4149be5
Mat Janson Blanchet <mat.jb@jansensan.net>
2020-10-20 18:32:41 -0400

    Ensured to not show citation number

---
+++ packages/databyss-services/citations/services/pruneCitation.js
@@ -0,0 +1,35 @@
+import CitationStyleIds from '../constants/CitationStyleIds'
+
+/**
+ * Trims the citation provided by CitationJS to remove the numbering.
+ * Used when only one citation is required.
+ * @param {string} citation A string of raw HTML provided by CitationJS.
+ * @param {*} styleId The identifier of the citation style.
+ */
+export function pruneCitation(citation, styleId) {
+  let response = citation
+
+  const div = document.createElement('div')
+  div.innerHTML = citation
+  const cslEntries = div.getElementsByClassName('csl-entry')
+
+  switch (styleId) {
+    case CitationStyleIds.APA:
+    case CitationStyleIds.CHICAGO:
+    case CitationStyleIds.MLA:
+      response = cslEntries[0].outerHTML
+      break
+
+    case CitationStyleIds.IEEE:
+    case CitationStyleIds.VANCOUVER:
+      const numberedEntry = cslEntries[0]
+      const lastIndex = numberedEntry.children.length - 1
+      const lastChild = numberedEntry.children[lastIndex]
+      response = lastChild.outerHTML
+      break
+  }
+
+  // TODO: remove class name and data attributes?
+
+  return response
+}
