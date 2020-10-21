44ec57983c752d61887508050b8ae875a1991f2f
Michael <michaelsenejoa@gmail.com>
2020-10-21 12:37:10 -0400

    preset

--- packages/databyss-api/webpack.config.js
+++ packages/databyss-api/webpack.config.js
@@ -49,10 +49,8 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: [
-              '@babel/preset-env',
-              '@babel/plugin-proposal-class-properties',
-            ],
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-class-properties'],
           },
         },
       },
