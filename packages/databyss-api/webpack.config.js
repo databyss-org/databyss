4bfdecbccfee7c0d365f8f8bb4c0377a810b3b54
Michael <michaelsenejoa@gmail.com>
2020-10-21 12:28:21 -0400

    webpack fix

--- packages/databyss-api/webpack.config.js
+++ packages/databyss-api/webpack.config.js
@@ -49,7 +49,10 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env'],
+            presets: [
+              '@babel/preset-env',
+              '@babel/plugin-proposal-class-properties',
+            ],
           },
         },
       },
