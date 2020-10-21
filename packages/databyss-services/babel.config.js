d9a5531e14199db50d32b514153272bf6742f455
Michael <michaelsenejoa@gmail.com>
2020-10-21 12:00:25 -0400

    babel

---
+++ packages/databyss-services/babel.config.js
@@ -0,0 +1,21 @@
+module.exports = api => {
+  api.cache(true)
+
+  const presets = ['@babel/preset-env', '@babel/preset-typescript']
+  const plugins = [
+    [
+      '@babel/plugin-transform-runtime',
+      {
+        regenerator: true,
+      },
+    ],
+    ['@babel/plugin-proposal-class-properties'],
+  ]
+  const ignore = ['**/node_modules']
+
+  return {
+    presets,
+    plugins,
+    ignore,
+  }
+}
