e86a6ca9c45ed0a0c4b3b9fa09d06aad2acaac1b
Mat Janson Blanchet <mat.jb@jansensan.net>
2020-11-02 12:13:09 -0500

    Added defensive checks to ensure generateCitation() exists

--- packages/databyss-ui/components/Citation/CitationLoader.js
+++ packages/databyss-ui/components/Citation/CitationLoader.js
@@ -3,28 +3,41 @@ import React from 'react'
 import { useCitationContext } from '@databyss-org/services/citations/CitationProvider'
 
 import { pxUnits } from '../../primitives'
+
 import MakeLoader from '../Loaders/MakeLoader'
 
 import Citation from './Citation'
 
 const CitationLoader = props => {
   const { sourceDetail, formatOptions } = props
-  const { generateCitation } = useCitationContext()
 
-  const render = () => (
-    <MakeLoader resources={generateCitation(sourceDetail, formatOptions)}>
-      {citation => (
-        <Citation
-          citation={citation}
-          formatOptions={formatOptions}
-          childViewProps={{
-            marginTop: pxUnits(20),
-            marginBottom: pxUnits(20),
-          }}
-        />
-      )}
-    </MakeLoader>
-  )
+  let generateCitation = null
+
+  const citationContext = useCitationContext()
+  if (citationContext) {
+    generateCitation = citationContext.generateCitation
+  }
+
+  const render = () => {
+    if (!generateCitation) {
+      return null
+    }
+
+    return (
+      <MakeLoader resources={generateCitation(sourceDetail, formatOptions)}>
+        {citation => (
+          <Citation
+            citation={citation}
+            formatOptions={formatOptions}
+            childViewProps={{
+              marginTop: pxUnits(20),
+              marginBottom: pxUnits(20),
+            }}
+          />
+        )}
+      </MakeLoader>
+    )
+  }
 
   return render()
 }
