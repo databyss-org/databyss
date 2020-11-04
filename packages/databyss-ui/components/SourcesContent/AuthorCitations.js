088125a73716a586946a0276272eb2f9ae51e190
Mat Janson Blanchet <mat.jb@jansensan.net>
2020-11-04 17:15:53 -0500

    Improved style for list of entries under author

--- packages/databyss-ui/components/SourcesContent/AuthorCitations.js
+++ packages/databyss-ui/components/SourcesContent/AuthorCitations.js
@@ -1,14 +1,38 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
+
+import { createIndexPageEntries } from '@databyss-org/services/entries/util'
 import {
-  sortEntriesAtoZ,
-  createIndexPageEntries,
-} from '@databyss-org/services/entries/util'
+  composeAuthorName,
+  isCurrentAuthor,
+} from '@databyss-org/services/sources/lib'
 import { SourceCitationsLoader } from '@databyss-org/ui/components/Loaders'
 import { useNavigationContext } from '@databyss-org/ui/components/Navigation/NavigationProvider/NavigationProvider'
-import SourceSvg from '@databyss-org/ui/assets/source.svg'
+
 import IndexPageContent from '../PageContent/IndexPageContent'
-import IndexPageEntries from '../PageContent/IndexPageEntries'
+
+import AuthorIndexEntries from './AuthorIndexEntries'
+
+function sortPageEntriesAlphabetically(entries) {
+  // error checks
+  if (!entries) {
+    throw new Error(
+      'sortPageEntriesAlphabetically() expected an array to sort,' +
+        'but none was provided'
+    )
+  }
+
+  // defensive checks
+  if (!entries.length) {
+    return entries
+  }
+
+  // sort
+  return entries.sort(
+    (a, b) =>
+      a.text.textValue.toLowerCase() > b.text.textValue.toLowerCase() ? 1 : -1
+  )
+}
 
 const AuthorCitations = ({ query }) => {
   const { navigate } = useNavigationContext()
@@ -16,74 +40,47 @@ const AuthorCitations = ({ query }) => {
   const params = new URLSearchParams(query)
   const authorQueryFirstName = params.get('firstName')
   const authorQueryLastName = params.get('lastName')
+  const authorFullName = composeAuthorName(
+    authorQueryFirstName,
+    authorQueryLastName
+  )
 
-  const composeAuthorName = (firstName, lastName) => {
-    if (firstName && lastName) {
-      return `${lastName}, ${firstName}`
-    }
-    return lastName || firstName
+  const onEntryClick = entry => {
+    navigate(`/sources/${entry.id}`)
   }
 
   return (
     <SourceCitationsLoader>
       {sourceCitations => {
-        // TODO: replace with packages/databyss-notes-mobile/utils/buildAuthorCitationData.js
-        const authorCitationsData = Object.values(sourceCitations).map(
-          value => {
-            const isCurrentAuthor = value.detail?.authors?.some(author => {
-              const firstName = author.firstName?.textValue
-              const lastName = author.lastName?.textValue
-              // If firstName or LastName is missing, only check the one defined
-              if (firstName === undefined && lastName) {
-                return lastName === authorQueryLastName
-              }
-              if (lastName === undefined && firstName) {
-                return firstName === authorQueryFirstName
-              }
-
-              return (
-                firstName === authorQueryFirstName &&
-                lastName === authorQueryLastName
-              )
-            })
-
-            if (isCurrentAuthor) {
-              return createIndexPageEntries({
+        const sourceEntries = []
+        const values = Object.values(sourceCitations)
+        values.forEach(value => {
+          const isAuthor = isCurrentAuthor(
+            value.detail?.authors,
+            authorQueryFirstName,
+            authorQueryLastName
+          )
+          if (isAuthor) {
+            sourceEntries.push(
+              createIndexPageEntries({
                 id: value._id,
-                text: value.text.textValue,
-                citations: value.detail?.citations?.map(
-                  citation => citation.text?.textValue
-                ),
-                type: 'sources',
+                text: value.text,
               })
-            }
-            return {}
+            )
           }
-        )
+        })
 
-        const sortedAuthorCitations = sortEntriesAtoZ(
-          authorCitationsData,
-          'text'
-        )
-
-        const onCitationClick = c => {
-          navigate(`/sources/${c.id}`)
-        }
+        const sortedSourceEntries = sortPageEntriesAlphabetically(sourceEntries)
 
         return (
-          <IndexPageContent
-            title={composeAuthorName(authorQueryFirstName, authorQueryLastName)}
-          >
+          <IndexPageContent title={authorFullName}>
             <Helmet>
               <meta charSet="utf-8" />
-              <title>
-                {composeAuthorName(authorQueryFirstName, authorQueryLastName)}
-              </title>
+              <title>{authorFullName}</title>
             </Helmet>
-            <IndexPageEntries
-              onClick={onCitationClick}
-              entries={sortedAuthorCitations}
-              icon={<SourceSvg />}
+            <AuthorIndexEntries
+              onClick={onEntryClick}
+              entries={sortedSourceEntries}
             />
           </IndexPageContent>
         )
