f0357b5413bf0260de1e612496345bfbee5d245e
pzhine <paul@hine.works>
2020-11-05 21:46:28 +0100

    copy sync-fetch dependency to app deploy dir

--- packages/databyss-api/webpack.config.js
+++ packages/databyss-api/webpack.config.js
@@ -35,7 +35,7 @@ module.exports = {
             __dirname,
             '../../node_modules/sync-fetch/worker.js'
           ),
-          to: '/worker.js',
+          to: path.resolve(__dirname, '../../worker.js'),
         },
       ],
     }),
