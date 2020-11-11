883c0b95d66cb51d8fdb67a952f3f4b4144e8e14
pzhine <paul@hine.works>
2020-11-11 22:27:12 +0100

    add indicies for account fields; manually add timestamps because if we let mongoose do it, we clobber the createdAt whenever we call block.saveWithDetail

---
+++ packages/databyss-api/src/lib/timestamps.js
@@ -0,0 +1,10 @@
+import ObjectID from 'bson-objectid'
+
+export const updateTimestamps = doc => {
+  const now = new Date()
+  if (!doc.createdAt) {
+    // extract the createdAt timestamp from the objectID of the doc
+    doc.createdAt = doc._id ? ObjectID(doc._id).getTimestamp() : now
+  }
+  doc.updatedAt = now
+}
