866a0dd922da84c564241092216b74e04e7058b2
Paul Hine <paul@hine.works>
2020-11-28 19:30:38 +0100

    debug rate control

--- packages/databyss-api/src/middleware/rateControlMiddleware.ts
+++ packages/databyss-api/src/middleware/rateControlMiddleware.ts
@@ -2,7 +2,6 @@ import ExpressSlowDown from 'express-slow-down'
 import RedisStore from 'rate-limit-redis'
 import { Express, Request } from 'express'
 import MurmurHash3 from 'imurmurhash'
-import { fail } from 'assert'
 
 export const createRateController = (app: Express) => {
   // Enable if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB, Nginx, etc)
@@ -10,51 +9,42 @@ export const createRateController = (app: Express) => {
   app.set('trust proxy', 1)
 
   // add success bit to keygen
-  const keyGenerator = (tag: string) => (req: Request) => {
-    const requestHash = MurmurHash3(
-      JSON.stringify([req.rawHeaders, req.method, req.url, req.body])
-    ).result()
-    const key = `${tag}:${req.ip}:${requestHash}`
-    console.log(key)
-    return key
-  }
+  // const keyGenerator = (tag: string) => (req: Request) => {
+  //   const requestHash = MurmurHash3(
+  //     JSON.stringify([req.rawHeaders, req.method, req.url, req.body])
+  //   ).result()
+  //   const key = `${tag}:${req.ip}:${requestHash}`
+  //   console.log(key)
+  //   return key
+  // }
 
-  const MemoryStore = require('express-slow-down/lib/memory-store')
-  const store =
-    process.env.NODE_ENV === 'production'
-      ? new RedisStore({
-          redisURL: process.env.REDIS_URL,
-        })
-      : // MemoryStore(windowMs) # how long to keep records of requests in memory
-        new MemoryStore(60 * 1000)
+  // const MemoryStore = require('express-slow-down/lib/memory-store')
+  // const store =
+  //   process.env.NODE_ENV === 'production'
+  //     ? new RedisStore({
+  //         redisURL: process.env.REDIS_URL,
+  //       })
+  //     : // MemoryStore(windowMs) # how long to keep records of requests in memory
+  //       new MemoryStore(60 * 1000)
 
   // limit successful requests (status < 400) to 10 req / sec / IP
-  const successLimiterConfig = {
-    windowMs: 1000,
-    delayAfter: 10,
-    delayMs: 50,
-    skipFailedRequests: true,
-    keyGenerator: keyGenerator('S'),
-    store,
-  }
+  // const successLimiterConfig = {
+  //   windowMs: 1000,
+  //   delayAfter: 10,
+  //   delayMs: 50,
+  //   skipFailedRequests: true,
+  //   keyGenerator: keyGenerator('S'),
+  //   store,
+  // }
 
   // limit failed requests (status >= 400) to 1 req / sec / IP
   const failedLimiterConfig = {
     windowMs: 1000,
     delayAfter: 1,
     delayMs: 1000,
-    skipSuccessfulRequests: true,
-    keyGenerator: keyGenerator('F'),
-    store,
-  }
-
-  // store to REDIS on production environments
-  if (process.env.NODE_ENV === 'production') {
-    const _store = new RedisStore({
-      redisURL: process.env.REDIS_URL,
-    })
-    failedLimiterConfig.store = _store
-    successLimiterConfig.store = _store
+    // skipSuccessfulRequests: true,
+    // keyGenerator: keyGenerator('F'),
+    // store,
   }
 
   // return [
