9bd1f519fc32cd0959fffcd4cd1c32b87d79277a
pzhine <paul.hine@gmail.com>
2020-12-11 19:40:15 +0100

    add counter to uid() fn

--- packages/databyss-data/lib/uid.ts
+++ packages/databyss-data/lib/uid.ts
@@ -1,9 +1,20 @@
 import Hashids from 'hashids'
 import { nanoid } from 'nanoid'
 
+interface Counter {
+  count: number
+}
+
 // databyss IDs
 // - are unique across all collections
 // - should encode a timestamp
-export const runtimeHash = nanoid()
-const hashids = new Hashids(runtimeHash)
-export const uid = () => hashids.encode(new Date().getTime())
+const _runtimeHash = nanoid()
+const _runtimeCounter = { count: 0 }
+const _hashids = new Hashids(_runtimeHash)
+export const uid = (
+  hashids: Hashids = _hashids,
+  counter: Counter = _runtimeCounter
+) => {
+  counter.count += 1
+  return hashids.encode(`${new Date().getTime()}${counter.count}`)
+}
