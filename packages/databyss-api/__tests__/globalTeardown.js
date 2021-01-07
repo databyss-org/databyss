0a661bdba9d954dde5a897319a657e9b80fbbe92
Michael <michaelsenejoa@gmail.com>
2021-01-07 17:25:40 -0500

    delete database

--- packages/databyss-api/__tests__/globalTeardown.js
+++ packages/databyss-api/__tests__/globalTeardown.js
@@ -1,6 +1,12 @@
 const { teardown: teardownDevServer } = require('jest-dev-server')
+const fetch = require('node-fetch')
 
 module.exports = async function globalTeardown() {
+  // clears cloudant database
+  await fetch(`${process.env.REACT_APP_API_URL}/cloudant`, {
+    method: 'DELETE',
+  })
+  console.log('DATABASE CLEARED')
   await teardownDevServer()
   // Your global teardown
   console.log('globalTeardown.js was invoked')
