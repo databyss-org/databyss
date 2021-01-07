a245bef91947fd30e3bb9d49d10784bca5c40abb
Michael <michaelsenejoa@gmail.com>
2021-01-07 17:44:23 -0500

    teardown

--- config/sauce/globalTeardown.js
+++ config/sauce/globalTeardown.js
@@ -1,7 +1,12 @@
 const { teardown } = require('../../scripts/sauce-dev-server')
+const fetch = require('node-fetch')
 
 module.exports = async function globalSetup() {
   if (!process.env.LOCAL_ENV) {
+    await fetch(`${process.env.REACT_APP_API_URL}/cloudant`, {
+      method: 'DELETE',
+    })
+    console.log('cloudant has been cleared')
     await teardown()
     console.log('SauceDevServer.teardown was invoked')
   }
