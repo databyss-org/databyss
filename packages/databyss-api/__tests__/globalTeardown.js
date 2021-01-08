15d25286e501240fda8ac62c01b47661610df3d9
Michael <michaelsenejoa@gmail.com>
2021-01-07 19:02:36 -0500

    uid

--- packages/databyss-api/__tests__/globalTeardown.js
+++ packages/databyss-api/__tests__/globalTeardown.js
@@ -1,12 +1,12 @@
 const { teardown: teardownDevServer } = require('jest-dev-server')
-const fetch = require('node-fetch')
+// const fetch = require('node-fetch')
 
 module.exports = async function globalTeardown() {
   // clears cloudant database
-  await fetch(`http://localhost:5000/api/cloudant/cloudant`, {
-    method: 'DELETE',
-  })
-  console.log('cloudant databse cleared')
+  // await fetch(`http://localhost:5000/api/cloudant/cloudant`, {
+  //   method: 'DELETE',
+  // })
+  // console.log('cloudant databse cleared')
   await teardownDevServer()
   // Your global teardown
   console.log('globalTeardown.js was invoked')
