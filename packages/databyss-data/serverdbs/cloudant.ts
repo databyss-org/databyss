70d30fc167afa55b9dae631f501eb78dbb36060e
Michael <michaelsenejoa@gmail.com>
2021-01-13 18:07:00 -0500

    cloudant retry

--- packages/databyss-data/serverdbs/cloudant.ts
+++ packages/databyss-data/serverdbs/cloudant.ts
@@ -3,7 +3,8 @@ import Cloudant from '@cloudant/cloudant'
 export const cloudant = Cloudant({
   account: process.env.CLOUDANT_USERNAME,
   password: process.env.CLOUDANT_PASSWORD,
-  plugins: 'promises',
+  maxAttempt: 5,
+  plugins: { retry: { retryErrors: false, retryStatusCodes: [429] } },
 })
 
 require('cloudant-upsert')(cloudant)
