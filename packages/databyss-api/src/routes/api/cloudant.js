e78cb95906102fb7deb4db86038941a11c855b4f
Michael <michaelsenejoa@gmail.com>
2021-01-19 16:59:20 -0500

    test fix

--- packages/databyss-api/src/routes/api/cloudant.js
+++ packages/databyss-api/src/routes/api/cloudant.js
@@ -1,7 +1,5 @@
 import express from 'express'
 import { cloudant } from '@databyss-org/data/couchdb/cloudant'
-// import { Users } from '@databyss-org/data/couchdb'
-import auth from '../../middleware/auth'
 import { UnauthorizedError } from '../../lib/Errors'
 
 const router = express.Router()
@@ -25,33 +23,6 @@ router.delete('/', async (req, res) => {
       console.log(`destroyed - ${_db}`)
     }
   }
-
-  return res.status(200).send()
-})
-
-// @route    POST api/cloudant/
-// @desc     Updates user preferences
-// @access   private
-router.post('/user', auth, async (req, res) => {
-  // const { session } = req.body.data
-  // user from auth token to user from session token
-  console.log('TODO ADD SYNC IN GROUP DB')
-  // if (session.userId === req.user.id) {
-  //   const _selector = {
-  //     selector: {
-  //       _id: { $eq: session.userId },
-  //     },
-  //   }
-  //   const _res = await Users.find(_selector)
-  //   if (!_res.docs.length) {
-  //     return res.status(401)
-  //   }
-  //   const user = _res.docs[0]
-  //   await Users.upsert(user._id, (oldDoc) => ({
-  //     ...oldDoc,
-  //     defaultPageId: session.defaultPageId,
-  //   }))
-  // }
 
   return res.status(200).send()
 })
