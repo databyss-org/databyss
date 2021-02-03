50110d669e2859546f5f0688a53078da259eeb13
Michael <michaelsenejoa@gmail.com>
2021-02-02 23:18:16 -0500

    test

--- packages/databyss-services/session/SessionProvider.js
+++ packages/databyss-services/session/SessionProvider.js
@@ -87,7 +87,7 @@ const SessionProvider = ({
     const _init = async () => {
       const _sesionFromLocalStorage = await localStorageHasSession()
       if (_sesionFromLocalStorage) {
-        await initiatePouchDbIndexes()
+        initiatePouchDbIndexes()
 
         // 2nd pass: load session from local_storage
         // replicate from cloudant
