6c926c5f1d0f753d22f81d85e5afcb92d35c163f
Michael <michaelsenejoa@gmail.com>
2021-02-16 14:24:43 -0500

    test

--- packages/databyss-data/pouchdb/entries/index.ts
+++ packages/databyss-data/pouchdb/entries/index.ts
@@ -1,5 +1,5 @@
-import getBlockRelations from './lib/getBlockRelations'
+
 import setBlockRelations from './lib/setBlockRelations'
 import searchEntries from './lib/searchEntries'
 
-export { getBlockRelations, setBlockRelations, searchEntries }
+export {  setBlockRelations, searchEntries }
