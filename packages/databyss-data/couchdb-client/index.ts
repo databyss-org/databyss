f56d56a79e004360a003deae54437715e4848de2
Paul Hine <paul@hine.works>
2021-03-03 19:04:22 +0100

    couchdb-client package WIP

--- packages/databyss-data/couchdb-client/index.ts
+++ packages/databyss-data/couchdb-client/index.ts
@@ -1,23 +1,31 @@
-import Slouch from 'couch-slouch'
+import request from '@databyss-org/services/lib/request'
+import { Base64 } from 'js-base64'
 import { getPouchSecret } from '@databyss-org/services/session/clientStorage'
 
-let slouch
+// export const couchDbRef = {
+//   current: null,
+// }
 
-export const init = async (groupId) => {
-  slouch = new Slouch(`https://${process.env.CLOUDANT_HOST}`)
-  console.log('couchdb-client.init', slouch)
-
+export const init = async (groupId?: string) => {
   // assume only one entry in pouch_secrets, so use first key as groupId
   // TODO: pass the groupId from session when we need multiple active logins
   //   or pull the groupId from the URL
   const _secrets = getPouchSecret()
   const _groupId = groupId || Object.keys(_secrets)[0]
-  const { dbKey: _username, dbPassword: _password } = _secrets
+  console.log('couchdb-client.init.groupId', _groupId)
+  const { dbKey: _username, dbPassword: _password } = _secrets[_groupId]
+  console.log('couchdb-client.init.user', _username)
+  console.log('couchdb-client.init.pass', _password)
   const _dbName = `g_${_groupId}`
-
-  await slouch.user.logIn(_username, _password)
+  const _doc = await request(
+    `https://${process.env.CLOUDANT_HOST}/${_dbName}/user_preference`,
+    {
+      headers: {
+        Authorization: `Basic ${Base64.btoa(`${_username}:${_password}`)}`,
+      },
+    }
+  )
 
   // log user_preferences to test connection
-  const doc = await slouch.doc.get(_dbName, 'user_preference')
-  console.log('couchdb-client.init', doc)
+  console.log('couchdb-client.init', _doc)
 }
