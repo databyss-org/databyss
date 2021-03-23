f2e94d09c2bd9c13d253b65d6e970558964a0645
pzhine <paul@hine.works>
2021-03-23 22:49:57 +0100

    only register the serviceWorker if in production env

--- packages/databyss-notes/serviceWorker.js
+++ packages/databyss-notes/serviceWorker.js
@@ -1,14 +1,14 @@
 import { Workbox } from 'workbox-window'
 
 export const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`
-export const workbox = new Workbox(swUrl)
 export const registrationRef = {
   current: null,
 }
 
 export function register() {
   if ('serviceWorker' in navigator) {
-    workbox.register().then((reg) => {
+    const _workbox = new Workbox(swUrl)
+    _workbox.register().then((reg) => {
       registrationRef.current = reg
     })
   }
