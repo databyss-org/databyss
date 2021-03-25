a9d2113137fdffd7c5c78d66d8f0c5d8656738aa
Michael <michaelsenejoa@gmail.com>
2021-03-25 16:01:32 -0400

    populate page error handler

--- packages/databyss-data/pouchdb/pages/lib/populatePage.ts
+++ packages/databyss-data/pouchdb/pages/lib/populatePage.ts
@@ -8,58 +8,62 @@ import { Selection } from '../../../../databyss-services/interfaces/Selection'
 const populatePage = async (
   _id: string
 ): Promise<Page | ResourceNotFoundError> => {
-  // TODO: wrap function in error handler
-  const _page: PageDoc | null = await getDocument(_id)
+  try {
+    const _page: PageDoc | null = await getDocument(_id)
 
-  if (!_page) {
+    if (!_page) {
+      return new ResourceNotFoundError('page not found')
+    }
+    // load selection
+    const _selection: Selection | null = await getDocument(_page.selection)
+
+    // coallesce page blocks into dict to filter duplicates and end blocks
+    const _blocksToGetDict = _page.blocks.reduce((accum, curr) => {
+      if (curr?.type?.match(/^END_/)) {
+        return accum
+      }
+      accum[curr._id] = curr
+      return accum
+    }, {})
+
+    // get all blocks in one request using bulk getDocuments
+    const _blocksDict = await getDocuments(Object.keys(_blocksToGetDict))
+
+    // populate blocks
+    const _blocks = _page.blocks.map((_pageBlock) => {
+      const _block = _blocksDict[_pageBlock._id]
+      if (_pageBlock.type?.match(/^END_/)) {
+        if (!_block) {
+          throw new Error(
+            `Can't find opener block for closer block with id: ${_pageBlock._id}`
+          )
+        }
+        return {
+          ..._pageBlock,
+          text: {
+            textValue: getAtomicClosureText(
+              _pageBlock.type,
+              _block.text.textValue
+            ),
+            ranges: [],
+          },
+        }
+      }
+      return _block
+    })
+
+    // add to blocks and selection to page
+    const _populatedPage: Page = {
+      ..._page,
+      selection: _selection!,
+      blocks: _blocks!,
+    }
+
+    return _populatedPage
+  } catch (err) {
+    console.error(err)
     return new ResourceNotFoundError('page not found')
   }
-  // load selection
-  const _selection: Selection | null = await getDocument(_page.selection)
-
-  // coallesce page blocks into dict to filter duplicates and end blocks
-  const _blocksToGetDict = _page.blocks.reduce((accum, curr) => {
-    if (curr?.type?.match(/^END_/)) {
-      return accum
-    }
-    accum[curr._id] = curr
-    return accum
-  }, {})
-
-  // get all blocks in one request using bulk getDocuments
-  const _blocksDict = await getDocuments(Object.keys(_blocksToGetDict))
-
-  // populate blocks
-  const _blocks = _page.blocks.map((_pageBlock) => {
-    const _block = _blocksDict[_pageBlock._id]
-    if (_pageBlock.type?.match(/^END_/)) {
-      if (!_block) {
-        throw new Error(
-          `Can't find opener block for closer block with id: ${_pageBlock._id}`
-        )
-      }
-      return {
-        ..._pageBlock,
-        text: {
-          textValue: getAtomicClosureText(
-            _pageBlock.type,
-            _block.text.textValue
-          ),
-          ranges: [],
-        },
-      }
-    }
-    return _block
-  })
-
-  // add to blocks and selection to page
-  const _populatedPage: Page = {
-    ..._page,
-    selection: _selection!,
-    blocks: _blocks!,
-  }
-
-  return _populatedPage
 }
 
 export default populatePage
