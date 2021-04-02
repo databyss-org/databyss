ae483420090fac752402a780094c79ae4612a6da
pzhine <paul@hine.works>
2021-04-02 22:08:20 +0200

    fix ts errors

--- packages/databyss-scripts/cmds/migrate_cmds/MigrateGroupDesignDocs.ts
+++ packages/databyss-scripts/cmds/migrate_cmds/MigrateGroupDesignDocs.ts
@@ -1,7 +1,6 @@
 import { cloudant } from '@databyss-org/data/couchdb/cloudant'
 import { run, ServerProcess, sleep } from '@databyss-org/scripts/lib'
-import { updateDesignDoc } from '@databyss-org/data/couchdb'
-import { DesignDoc } from '@databyss-org/data/interfaces'
+import { updateDesignDocs } from '@databyss-org/data/couchdb'
 
 export class MigrateGroupDesignDocs extends ServerProcess {
   constructor(argv) {
@@ -14,8 +13,7 @@ export class MigrateGroupDesignDocs extends ServerProcess {
       if (!_dbName.startsWith('g_') && !_dbName.startsWith('p_')) {
         continue
       }
-      const _db = cloudant.current.db.use<DesignDoc>(_dbName)
-      await updateDesignDoc({ db: _db })
+      await updateDesignDocs()
       // dont exceed cloudant rate limit
       await sleep(100)
       this.log(`⬆️  migrated: ${_dbName}`)
