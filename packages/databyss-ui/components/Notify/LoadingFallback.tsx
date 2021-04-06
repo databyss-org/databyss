d5b2ca90297d48d718dc43d38ae0c1785e2b3cf6
Paul Hine <paul@hine.works>
2021-04-06 14:55:44 +0200

    paul/wait-longer (#706)
    * Migrate NotifyProvider to TS
    
    * Please wait longer message
    
    * don't show wait longer messages in TEST env
    
    * don't show wait longer messages in TEST env
    
    * LoadingFallback should work without NotifyProvider
    
    * prevent long wait message from showing until final login step
    
    * tweak longer language

--- packages/databyss-ui/components/Notify/LoadingFallback.tsx
+++ packages/databyss-ui/components/Notify/LoadingFallback.tsx
@@ -1,22 +1,62 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import LoadingIcon from '@databyss-org/ui/assets/loading.svg'
 import { View, ViewProps } from '@databyss-org/ui/primitives'
 import { QueryObserverResult } from 'react-query'
+import { useNotifyContext, DialogOptions } from './NotifyProvider'
 import { ErrorText } from './ErrorText'
 
 interface LoadingFallbackProps extends ViewProps {
   /**
-   * Size of the loading icon
+   * Size of the loading icon.
    */
   size?: string
+  /**
+   * The react-query queryObserver this loader is waiting on
+   */
   queryObserver?: QueryObserverResult | QueryObserverResult[]
+  /**
+   * If true, show a @longWaitMessage after @longWaitMs
+   */
+  showLongWaitMessage?: boolean
+  /**
+   * Milliseconds before the longWaitMessage is shown
+   */
+  longWaitMs?: number
+  /**
+   * Text to show if more than @longWaitMs ms has elapsed
+   */
+  longWaitDialogOptions?: DialogOptions
 }
 
 export const LoadingFallback = ({
   size,
   queryObserver,
+  longWaitMs,
+  showLongWaitMessage,
+  longWaitDialogOptions,
   ...others
 }: LoadingFallbackProps) => {
+  const { notify, hideDialog } = useNotifyContext() ?? {}
+  const timerRef = useRef<number | null>(null)
+
+  useEffect(() => {
+    if (process.env.NODE_ENV === 'test' || !showLongWaitMessage) {
+      return () => null
+    }
+    timerRef.current = setTimeout(() => {
+      notify({
+        showConfirmButtons: false,
+        ...longWaitDialogOptions,
+      })
+    }, longWaitMs)
+    return () => {
+      if (timerRef.current) {
+        hideDialog()
+        clearTimeout(timerRef.current!)
+      }
+    }
+  }, [])
+
   let _queryObservers: QueryObserverResult[] | null = null
   if (queryObserver) {
     _queryObservers = Array.isArray(queryObserver)
@@ -45,6 +85,14 @@ export const LoadingFallback = ({
 
 LoadingFallback.defaultProps = {
   size: 25,
+  showLongWaitMessage: false,
+  longWaitMs: 2500,
+  longWaitDialogOptions: {
+    message:
+      "<strong>Databyss is synching with the cloud.</strong><p>If it's not done shortly...</p><p>&nbsp;&nbsp;&nbsp;...just wait longer.</p>",
+    html: true,
+    dolphins: true,
+  },
 }
 
 export default LoadingFallback
