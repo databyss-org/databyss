15ef0d9ba67734080f492d6f197180395fc202d1
Paul Hine <paul@hine.works>
2021-10-24 13:01:18 +0200

    try find-remove package to clean backup dirs

--- packages/databyss-scripts/cmds/backup_cmds/Backup.ts
+++ packages/databyss-scripts/cmds/backup_cmds/Backup.ts
@@ -6,14 +6,28 @@ import {
   sleep,
 } from '@databyss-org/scripts/lib'
 import { cloudant } from '@databyss-org/data/couchdb/cloudant'
+import yargs from 'yargs'
+import findRemoveSync from 'find-remove'
 import { BackupDb } from './BackupDb'
 import { fileFriendlyDateTime } from '../../lib/ServerProcess'
+
+const SECONDS_PER_DAY = 86400
 
 export class Backup extends ServerProcess {
   constructor(argv: ServerProcessArgs) {
     super(argv, 'backup.instance')
   }
   async run() {
+    this.logInfo('Clean?', this.args.clean)
+    if (this.args.clean) {
+      findRemoveSync(this.args.path, {
+        age: {
+          seconds: this.args.clean * SECONDS_PER_DAY,
+        },
+        prefix: 'instance_',
+        limit: 100,
+      })
+    }
     const outputPath = path.join(
       this.args.path,
       `instance_${fileFriendlyDateTime()}`
@@ -40,7 +54,16 @@ export class Backup extends ServerProcess {
 
 exports.command = 'instance [options]'
 exports.desc = 'Backup all dbs in a cloudant instance'
-exports.builder = {}
+exports.builder = (yargs: ServerProcessArgs) =>
+  yargs.describe(
+    'clean',
+    'Remove instance backups from output path directory that are older than N days'
+  )
+yargs.example(
+  '$0 backup --clean=7',
+  'Removes backups older than 7 days before running the backup'
+)
+yargs.number('clean')
 exports.handler = (argv: ServerProcessArgs) => {
   new Backup(argv).runCli()
 }
