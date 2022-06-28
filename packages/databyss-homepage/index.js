af594709a4ec4affa82717b0512b50d75b06ba5e
Paul Hine <paul@hine.works>
2022-06-28 14:36:16 +0200

    embed donorbox form on mobile

--- packages/databyss-homepage/index.js
+++ packages/databyss-homepage/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Helmet } from 'react-helmet'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { ThemeProvider } from '@databyss-org/ui/theming'
 import { Page } from './components/Page'
 import homepageContent from './content/homepageContent.json'
 import foundationContent from './content/foundationContent.json'
+import { EmbedDonorbox, DonorboxPopup } from './components/Donorbox'
 
 const globalStyles = () => ({
   '& a, & a:visited': {
@@ -22,17 +22,10 @@ ReactDOM.render(
           path="/foundation"
           element={<Page content={foundationContent} />}
         />
+        <Route path="/donate" element={<EmbedDonorbox />} />
       </Routes>
     </BrowserRouter>
-    <Helmet>
-      <script
-        type="text/javascript"
-        defer
-        src="https://donorbox.org/install-popup-button.js"
-      />
-    </Helmet>
+    <DonorboxPopup />
   </ThemeProvider>,
   document.getElementById('root')
 )
-
-window.DonorBox = { widgetLinkClassName: 'custom-dbox-popup' }
