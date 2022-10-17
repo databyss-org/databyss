2564f980b37e295321de2a47ce2a07dd6bbd9c32
Paul Hine <paul@hine.works>
2022-10-17 12:39:14 +0200

    leave test dbs intact

--- config/sauce/globalTeardown.js
+++ config/sauce/globalTeardown.js
@@ -1,13 +1,13 @@
-const fetch = require('node-fetch')
+// const fetch = require('node-fetch')
 const { teardown } = require('../../scripts/sauce-dev-server')
 
 module.exports = async function globalSetup() {
   if (!process.env.LOCAL_ENV) {
     // clears cloudant database
 
-    await fetch(`http://0.0.0.0:5050/api/cloudant/`, {
-      method: 'DELETE',
-    })
+    // await fetch(`http://0.0.0.0:5050/api/cloudant/`, {
+    //   method: 'DELETE',
+    // })
     await teardown()
     console.log('SauceDevServer.teardown was invoked')
   }
