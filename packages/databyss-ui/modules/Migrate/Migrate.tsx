e41d52537f2f639edef2bdbc356e4b9b72c2f3e8
Paul Hine <paul.hine@gmail.com>
2026-09-03 02:01:41 +0200

    replace app with migration page

---
+++ packages/databyss-ui/modules/Migrate/Migrate.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { RawHtml, Text, View } from '../..'
+import defaultContent from './migrate.json'
+
+interface MigrateContent {
+  title: string
+  body: string
+}
+
+export const Migrate = ({
+  content = defaultContent,
+}: {
+  content?: MigrateContent
+}) => (
+  <View
+    widthVariant="form"
+    alignItems="left"
+    flexGrow={1}
+    justifyContent="center"
+    mb="extraLarge"
+  >
+    <Text variant="heading2" color="gray.3" mb="medium">
+      {content.title}
+    </Text>
+
+    <RawHtml variant="uiTextNormal" html={content.body} />
+  </View>
+)
